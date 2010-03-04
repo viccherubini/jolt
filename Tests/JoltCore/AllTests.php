58a772853b9d5092ac66f1004fdd5203a67603ec
Vic Cherubini <vmc@leftnode.com>
2010-03-04 06:48:11 -0600

    Added a new test suite for the route tests.

--- Tests/JoltCore/AllTests.php
+++ Tests/JoltCore/AllTests.php
@@ -3,14 +3,15 @@
 require_once 'PHPUnit/Framework.php';
 require_once 'Router/RouterTest.php';
 require_once 'Controller/ControllerTest.php';
+require_once 'Route/AllTests.php';
 
 class JoltCore_AllTests {
 	public static function suite() {
 		$suite = new PHPUnit_Framework_TestSuite('JoltCore');
 		$suite->addTestSuite('JoltCore_Router_RouterTest');
 		$suite->addTestSuite('JoltCore_Controller_ControllerTest');
-		
+		$suite->addTestSuite(JoltCore_Route_AllTests::suite());
+
 		return $suite;
 	}
-
 }
