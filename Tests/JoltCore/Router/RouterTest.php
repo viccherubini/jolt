39215248b83b7281a0fa200de1ba9302f0ef777e
Vic Cherubini <vmc@leftnode.com>
2010-03-04 06:50:06 -0600

    New tests for the router.

--- Tests/JoltCore/Router/RouterTest.php
+++ Tests/JoltCore/Router/RouterTest.php
@@ -1,11 +1,23 @@
 <?php
 
 require_once 'PHPUnit/Framework.php';
+require_once 'Jolt/Router.php';
 
 class JoltCore_Router_RouterTest extends PHPUnit_Framework_TestCase {
 
-	public function testEmptyRouter() {
-		$router = array();
-		$this->assertEquals(0, count($router));
+	public function testEmptyNamedRouteList() {
+		$router = new Router();
+		
+		$this->assertEquals(is_array(array()), is_array($router->getNamedRouteList()));
+		$this->assertEquals(0, count($router->getNamedRouteList()));
 	}
+	
+	public function testEmptyRestfulRouteList() {
+		$router = new Router();
+		
+		$this->assertEquals(is_array(array()), is_array($router->getRestfulRouteList()));
+		$this->assertEquals(0, count($router->getRestfulRouteList()));
+	}
+	
+	
 }
