0e35c6fe4bb96342b65773dfbec3f097bd578cce
Vic Cherubini <vmc@leftnode.com>
2010-03-04 06:49:13 -0600

    New tests for the Controller class. Surprisingly building an abstract mock object counts as an assertion.

--- Tests/JoltCore/Controller/ControllerTest.php
+++ Tests/JoltCore/Controller/ControllerTest.php
@@ -1,16 +1,20 @@
 <?php
 
 require_once 'PHPUnit/Framework.php';
-require_once 'Jolt/Jolt.php';
+require_once 'Jolt/Controller.php';
 
 class JoltCore_Controller_ControllerTest extends PHPUnit_Framework_TestCase {
-
-	public function testEmptyController() {
-		$controller = array();
-		$this->assertEquals(0, count($controller));
+	
+	public function testConfigIsEmpty() {
+		$controller = $this->buildController();
+		
+		$this->assertEquals(is_array(array()), is_array($controller->getConfig()));
+		$this->assertEquals(0, count($controller->getConfig()));
 	}
 
-	public function testAisA() {
-		$this->assertEquals('a', 'a');
+
+	protected function buildController() {
+		$controller = $this->getMockForAbstractClass('Jolt_Controller');
+		return $controller;
 	}
 }
