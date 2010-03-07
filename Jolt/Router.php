7534492cb3217b5343a49fa638a679efdabaaba9
Vic Cherubini <vmc@leftnode.com>
2010-03-07 06:54:20 -0600

    Added a few methods to manage the configuration (getters/setters) and the main dispatch() method to actually do the routing. Wrote tests for them as well.

--- Jolt/Router.php
+++ Jolt/Router.php
@@ -7,6 +7,7 @@ require_once 'Exception.php';
 
 class Router {
 
+	private $config = array();
 	private $named_list = array();
 	private $restful_list = array();
 
@@ -18,13 +19,41 @@ class Router {
 		
 	}
 	
+	public function setConfig(array $config) {
+		if ( 0 === count($config) ) {
+			throw new \Jolt\Exception('router_config_empty');
+		}
+		
+		/**
+		 * Ensure the fields below are in the config array.
+		 * No other fields should be present.
+		 */
+		$required_fields = array(
+			'site_root' => true,
+			'site_root_secure' => true,
+			'app_dir' => true,
+			'layout_dir' => true,
+			'default_layout' => true,
+			'rewrite' => true
+		);
+		
+		$field_difference = array_diff_key($required_fields, $config);
+		
+		if ( count($field_difference) > 0 ) {
+			throw new \Jolt\Exception('router_config_malformed');
+		}
+		
+		$this->config = $config;
+		return $this;
+	}
+	
 	public function setNamedRouteList(array $named_list) {
 		if ( 0 === count($named_list) ) {
-			throw new Jolt_Exception('router_named_list_empty');
+			throw new \Jolt\Exception('router_named_list_empty');
 		}
 		
 		array_walk($named_list, function(&$v, $k) {
-			if ( false === $v instanceof Jolt_Route ) {
+			if ( false === $v instanceof \Jolt\Route ) {
 				throw new \Jolt\Exception('router_named_list_element_not_route');
 			}
 		});
@@ -34,6 +63,13 @@ class Router {
 		
 	}
 	
+	
+	
+	
+	public function getConfig() {
+		return (array)$this->config;
+	}
+	
 	public function getNamedRouteList() {
 		return (array)$this->named_list;
 	}
@@ -41,6 +77,20 @@ class Router {
 	public function getRestfulRouteList() {
 		return (array)$this->restful_list;
 	}
+	
+	
+	
+	public function dispatch() {
+		$named_list = $this->getNamedRouteList();
+		$restful_list = $this->getRestfulRouteList();
+		
+		if ( 0 === count($named_list) && 0 === count($restful_list) ) {
+			throw new \Jolt\Exception('router_lists_empty');
+		}
+		
+		
+	}
+	
 	
 	private function reset() {
 		$this->named_list = array();
