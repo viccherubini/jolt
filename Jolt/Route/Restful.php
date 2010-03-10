2fdc04ab6abbc8b5e1a79fc2b271b888d04e97d8
Vic Cherubini <vmc@leftnode.com>
2010-03-10 06:58:31 -0600

    Added the set/getResource() methods.

--- Jolt/Route/Restful.php
+++ Jolt/Route/Restful.php
@@ -6,7 +6,31 @@ namespace Jolt;
 require_once 'Jolt/Route.php';
 
 class Route_Restful extends \Jolt\Route {
-
+	
+	private $resource = NULL;
+	
+	public function __construct($route, $resource) {
+		$this->setRoute($route)
+			->setResource($resource);
+	}
+	
+	
+	public function setResource($resource) {
+		$resource = trim($resource);
+		if ( true === empty($resource) ) {
+			throw new \Jolt\Exception('route_restful_resource_empty');
+		}
+		
+		$this->resource = $resource;
+		return $this;
+	}
+	
+	
+	
+	
+	public function getResource() {
+		return $this->resource;
+	}
 	
 	public function isValid() {
 		return false;
