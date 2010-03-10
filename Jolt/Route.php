bbcefcbd90dad6d5fdf26c85d816f52c0098be64
Vic Cherubini <vmc@leftnode.com>
2010-03-10 06:57:42 -0600

    Moved the setting and getting of the route to the parent \Jolt\Route class.

--- Jolt/Route.php
+++ Jolt/Route.php
@@ -5,7 +5,7 @@ namespace Jolt;
 
 abstract class Route {
 
-	
+	private $route = NULL;
 
 	public function __construct() {
 		
@@ -15,7 +15,20 @@ abstract class Route {
 		
 	}
 	
-
+	public function setRoute($route) {
+		$route = trim($route);
+		if ( true === empty($route) ) {
+			throw new \Jolt\Exception('route_empty');
+		}
+		
+		$this->route = $route;
+		
+		return $this;
+	}
+	
+	public function getRoute() {
+		return $this->route;
+	}
 	
 	abstract public function isValid();
 }
