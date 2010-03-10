8a59568b963bb9d354222ac1d1db7cad458d046f
Vic Cherubini <vmc@leftnode.com>
2010-03-10 06:55:54 -0600

    Started to write the tests for the Restful routes.

--- Tests/JoltCore/Route/Route/RestfulTest.php
+++ Tests/JoltCore/Route/Route/RestfulTest.php
@@ -1,11 +1,35 @@
 <?php
 
 namespace JoltTest;
+use \Jolt\Route_Restful;
+
+require_once 'Jolt/Route/Restful.php';
 
 class JoltCore_Route_Route_RestfulTest extends TestCase {
+	/**
+	 * @expectedException \Jolt\Exception
+	 */
+	public function testRouteCanNotBeEmpty() {
+		$route = new Route_Restful(NULL, 'Resource');
+	}
 	
-	public function testInit() {
-		$this->assertEquals(1, 1);
+	/**
+	 * @expectedException \Jolt\Exception
+	 */
+	public function testRouteMustHaveResource() {
+		$route = new Route_Restful('/user', NULL);
 	}
 
+	public function testRouteIsSet() {
+		$route = new Route_Restful('/user', 'Resource');
+		$this->assertEquals('/user', $route->getRoute());
+	}
+	
+	
+	public function testResourceIsSet() {
+		$route = new Route_Restful('/user', 'Resource');
+		$this->assertEquals('Resource', $route->getResource());
+	}
+	
+	
 }
