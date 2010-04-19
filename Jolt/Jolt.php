fcefbd5b780cfa45149c876bb29e01cf2fb88f34
Vic Cherubini <vmc@leftnode.com>
2010-04-19 07:03:31 -0500

    Starting to build the main Jolt class for a central application API.

--- Jolt/Jolt.php
+++ Jolt/Jolt.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+ * This class is an entrance for building an application. It is entirely
+ * static and gives you access to building new objects, starting the 
+ * application, and basic CSRF protection.
+ * 
+ * @author vmc <vmc@leftnode.com>
+ */
 class Jolt {
+	//private static $named_route_list = array();
+	//private static $restful_route_list = array();
+	
+	public static function setRouter(Jolt_Router $router) {
+		
+	}
+	
+	//public static function setNamedRouteList($route_list) {
+	//	
+	//}
+	
+	//public static function setRestfulRouteList($route_list) {
+	//	
+	//}
+	
+	
+	public static function buildJoltObject($object) {
+		
+	}
+	
+	
 
 }
