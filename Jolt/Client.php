b0c048aefb8a9a7a6988a8ef26dbe4da26c26569
Vic Cherubini <vmc@leftnode.com>
2010-04-20 05:46:06 -0500

    New Jolt_Client object for sending data and headers back to the actual client.

---
+++ Jolt/Client.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * The client is what is responsible for sending rendered views, errors,
+ * and/or headers back to the actual client. It should generally be initiated
+ * by a Jolt_Dispatcher and not directly.
+ * 
+ * @author vmc <vmc@leftnode.com>
+ */
+class Jolt_Client {
+
+}
