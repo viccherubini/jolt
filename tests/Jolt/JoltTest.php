fd1cc5a9f6289dd21243a289e4469ef483a0e940
Vic Cherubini <vmc@leftnode.com>
2010-04-20 05:22:13 -0500

    New test for the static class Jolt.

---
+++ tests/Jolt/JoltTest.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'Jolt/Jolt.php';
+
+class Jolt_JoltTest extends Jolt_TestCase {
+	
+	/**
+	 * @expectedException Jolt_Exception
+	 */
+	public function test_Attached_Router_Has_At_Least_One_Route() {
+		$router = $this->buildEmptyRouter();
+		Jolt::attachRouter($router);
+	}
+	
+	
+	
+	
+	
+	protected function buildEmptyRouter() {
+		$mock = $this->getMock('Jolt_Router', array('getRouteCount'));
+		
+		$mock->expects($this->any())
+			->method('getRouteCount')
+			->will($this->returnValue(0));
+		
+		return $mock;
+	}
+	
+	protected function buildRouter() {
+		$named_route_list = array($this->buildNamedRoute('/abc', 'Controller', 'action'));
+		
+		$mock = $this->getMock('Jolt_Router', array('getRouteCount'));
+		
+		$mock->expects($this->any())
+			->method('getRouteCount')
+			->will($this->returnValue(count($named_route_list)));
+		
+		return $mock;
+	}
+	
+}
