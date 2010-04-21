9731fd13788ec5938b0661bc7320830f5b5e2430
Vic Cherubini <vmc@leftnode.com>
2010-04-21 06:00:21 -0500

    Added the dispatcher to the test suite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -9,6 +9,7 @@ require_once 'PHPUnit/Framework.php';
 require_once 'TestCase.php';
 
 require_once 'Controller/ControllerTest.php';
+require_once 'Dispatcher/DispatcherTest.php';
 require_once 'Jolt/JoltTest.php';
 require_once 'Registry/RegistryTest.php';
 require_once 'Route/AllTests.php';
@@ -20,6 +21,7 @@ class Jolt_AllTests {
 		$suite = new PHPUnit_Framework_TestSuite('Jolt All Tests');
 		
 		$suite->addTestSuite('Jolt_Controller_ControllerTest');
+		$suite->addTestSuite('Jolt_Dispatcher_DispatcherTest');
 		$suite->addTestSuite('Jolt_JoltTest');
 		$suite->addTestSuite('Jolt_Registry_RegistryTest');
 		$suite->addTestSuite(Jolt_Route_AllTests::suite());
