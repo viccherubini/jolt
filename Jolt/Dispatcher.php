2219081ee05f017953b7cba4afb72905db471beb
Vic Cherubini <vmc@leftnode.com>
2010-04-21 05:59:46 -0500

    The new Jolt_Dispatcher class for taking a matched route and loading up the Controller and action it uses.

--- Jolt/Dispatcher.php
+++ Jolt/Dispatcher.php
@@ -2,4 +2,44 @@
 
 class Jolt_Dispatcher {
 	
+	private $controller_path = NULL;
+	
+	private $route = NULL;
+	
+	public function __construct() {
+		
+	}
+	
+	public function dispatch() {
+		$route = $this->getRoute();
+		if ( NULL === $route ) {
+			throw new Jolt_Exception('dispatcher_route_is_null');
+		}
+		
+		
+	}
+	
+	public function getControllerPath() {
+		return $this->controller_path;
+	}
+	
+	public function getRoute() {
+		return $this->route;
+	}
+	
+	
+	
+	
+	
+	public function setControllerPath($controller_path) {
+		$this->controller_path = trim($controller_path);
+		return $this;
+	}
+	
+	public function setRoute(Jolt_Route $route) {
+		$this->route = $route;
+		return $this;
+	}
+	
+	
 }
