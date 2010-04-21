ad830a0314e7b49919d8a882c9f3496c080474f1
Vic Cherubini <vmc@leftnode.com>
2010-04-21 05:58:59 -0500

    New test for Jolt_Dispatcher.

---
+++ tests/Dispatcher/DispatcherTest.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'Jolt/Dispatcher.php';
+
+class Jolt_Dispatcher_DispatcherTest extends Jolt_TestCase {
+	
+	public function test_Dispatcher_Controller_Path_Is_Set() {
+		$dispatcher = new Jolt_Dispatcher();
+		
+		$controller_path = getcwd();
+		$dispatcher->setControllerPath($controller_path);
+		
+		$this->assertEquals($controller_path, $dispatcher->getControllerPath());
+	}
+	
+	public function test_Route_Is_Set() {
+		$dispatcher = new Jolt_Dispatcher();
+		
+		$route = $this->buildNamedRoute('/user/%n', 'User', 'viewAction');
+		$dispatcher->setRoute($route);
+		
+		$this->assertTrue($route->isEqual($dispatcher->getRoute()));
+	}
+	
+	/**
+	 * @expectedException Jolt_Exception
+	 */
+	public function test_Dispatcher_Must_Have_Route_Before_Executing() {
+		$dispatcher = new Jolt_Dispatcher();
+		$dispatcher->dispatch();
+	}
+}
