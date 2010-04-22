98f102701dd0f04c37770482c60edabaeafb35aa
Vic Cherubini <vmc@leftnode.com>
2010-04-22 06:20:32 -0500

    Added some more getter/setter methods to handle configuration properties of the dispatcher.

--- Jolt/Dispatcher.php
+++ Jolt/Dispatcher.php
@@ -2,7 +2,11 @@
 
 class Jolt_Dispatcher {
 	
+	private $application_path = NULL;
+	
 	private $controller_path = NULL;
+	
+	private $layout_path = NULL;
 	
 	private $route = NULL;
 	
@@ -17,13 +21,32 @@ class Jolt_Dispatcher {
 		}
 		
 		$controller_file = $route->getControllerFile();
-		if ( true === empty($controller_file) ) {
-			throw new Jolt_Exception('dispatcher_route_controller_file_is_null');
-		}
+		lib_throw_if(empty($controller_file), 'dispatcher_route_controller_file_is_null');
+		
+		$application_path = $this->getApplicationPath();
+		lib_throw_if(empty($application_path), 'dispatcher_application_path_is_null');
+		
+		$controller_path = $this->getControllerPath();
+		lib_throw_if(empty($application_path), 'dispatcher_controller_path_is_null');
+		
+		$ds = DIRECTORY_SEPARATOR;
+		$execution_path = $application_path . $ds . $controller_path . $ds . $controller_file;
+		
+		
+	}
+	
+	
+	
+	public function getApplicationPath() {
+		return $this->application_path;
 	}
 	
 	public function getControllerPath() {
 		return $this->controller_path;
+	}
+	
+	public function getLayoutPath() {
+		return $this->layout_path;
 	}
 	
 	public function getRoute() {
@@ -33,9 +56,18 @@ class Jolt_Dispatcher {
 	
 	
 	
+	public function setApplicationPath($application_path) {
+		$this->application_path = rtrim($application_path, DIRECTORY_SEPARATOR);
+		return $this;
+	}
 	
 	public function setControllerPath($controller_path) {
-		$this->controller_path = trim($controller_path);
+		$this->controller_path = rtrim($controller_path, DIRECTORY_SEPARATOR);
+		return $this;
+	}
+	
+	public function setLayoutPath($layout_path) {
+		$this->layout_path = rtrim($layout_path, DIRECTORY_SEPARATOR);
 		return $this;
 	}
 	
