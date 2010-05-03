4191d282ff8b77e1fe862fc9ef4ba3e128a460fe
Vic Cherubini <vmc@leftnode.com>
2010-05-03 03:19:45 -0500

    And the tests that go with them.

--- tests/View/ViewTest.php
+++ tests/View/ViewTest.php
@@ -4,4 +4,20 @@ require_once 'Jolt/View.php';
 
 class Jolt_View_ViewTest extends Jolt_TestCase {
 	
+	public function test_View_Application_Path_Is_Set() {
+		$view = new Jolt_View();
+		$view->setApplicationPath(getcwd());
+		
+		$this->assertEquals(getcwd(), $view->getApplicationPath());
+	}
+	
+	public function test_View_Block_Directory_Is_Set() {
+		$view = new Jolt_View();
+		$view->setBlockDirectory('blocks/'); // The / should be trimmed off
+		
+		$this->assertEquals('blocks', $view->getBlockDirectory());
+	}
+	
+	
+	
 }
