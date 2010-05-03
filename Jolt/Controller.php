6f50f47b9e6f6a969b400827afe86268bcc3a5f5
Vic Cherubini <vmc@leftnode.com>
2010-05-03 03:04:02 -0500

    Working on the controller to get/set the layout name.

--- Jolt/Controller.php
+++ Jolt/Controller.php
@@ -2,6 +2,10 @@
 
 abstract class Jolt_Controller {
 	
+	private $layout = NULL;
+	
+	private $layout_object = NULL;
+	
 	private $config = array();
 	
 	public function __construct() {
@@ -11,13 +15,37 @@ abstract class Jolt_Controller {
 	public function __destruct() {
 		
 	}
+
+	public function render($view, $block=NULL) {
+		
+		
+	}
+	
+	
+	
+
+	public function getConfig() {
+		return $this->config;
+	}
+	
+	public function getLayout() {
+		return $this->layout;
+	}
+	
+	
+	
 	
 	public function setConfig(array $config) {
 		$this->config = $config;
 		return $this;
 	}
 	
-	public function getConfig() {
-		return $this->config;
+	
+	public function setLayout($layout) {
+		$this->layout = $layout;
+		return $this;
 	}
+	
+	
+	
 }
--- tests/Controller/ControllerTest.php
+++ tests/Controller/ControllerTest.php
@@ -4,11 +4,21 @@ require_once 'Jolt/Controller.php';
 
 class Jolt_Controller_ControllerTest extends Jolt_TestCase {
 	
-	public function testConfigIsEmpty() {
+	public function test_Controller_Config_Is_Empty() {
 		$controller = $this->buildController();
 		$this->assertArray($controller->getConfig());
 		$this->assertEmptyArray($controller->getConfig());
 	}
+	
+	
+	public function test_Controller_Layout_Is_Set() {
+		$controller = $this->buildController();
+		$controller->setLayout('default');
+		
+		$this->assertEquals('default', $controller->getLayout());
+	}
+	
+	
 
 	protected function buildController() {
 		$controller = $this->getMockForAbstractClass('Jolt_Controller');
