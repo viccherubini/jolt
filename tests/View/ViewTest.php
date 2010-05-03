735d45704065fa7abb734bcf534a200d1675f206
Vic Cherubini <vmc@leftnode.com>
2010-05-03 04:23:52 -0500

    Wrote the basic provider and test method for testing to ensure a view renders properly. Tests no variable replacement, single variable replacement, multiple variable replacement, array replacement, and object replacement. Within the application/view/ directory exist several pre-rendered views that show what the rendering should ultimately look like. These files are opened and then compared to the actual rendering.

--- tests/View/ViewTest.php
+++ tests/View/ViewTest.php
@@ -19,25 +19,55 @@ class Jolt_View_ViewTest extends Jolt_TestCase {
 	}
 	
 	/**
-	 * @dataProvider providerViewVariable
+	 * @dataProvider providerReplacementList
 	 */
 	public function test_View_Magic_Getter_And_Setter($k, $v) {
 		$view = new Jolt_View();
 		$view->$k = $v;
 		
-		$value_list = array($k => $v);
+		$this->assertEquals(array($k => $v), $view->getReplacementList());
+	}
+	
+	/**
+	 * @dataProvider providerRenderableViewList
+	 */
+	public function test_View_Renders_When_View_File_Exists($view_name, $replacement_list) {
+		$view = new Jolt_View();
 		
-		$this->assertEquals($value_list, $view->getVariableList());
+		$app_path = rtrim(getcwd(), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . APPLICATION_DIRECTORY;
+		
+		$view->setApplicationPath($app_path)->setReplacementList($replacement_list);
+		$view->render($view_name);
+		
+		/* Cheap way to get the rendered view file. */
+		$view_file = $view->getViewFile();
+		$view_file_rendered = str_replace(Jolt_View::VIEW_EXT, '-rendered' . Jolt_View::VIEW_EXT, $view_file);
+		
+		$this->assertEquals(file_get_contents($view_file_rendered), $view->getRendering());
 	}
 	
 	
 	
 	
-	public function providerViewVariable() {
+	public function providerReplacementList() {
 		return array(
 			array('variable', 'value'),
 			array('array_variable', array(1, 2, 3, 4, 5)),
 			array('object_variable', new stdClass())
+		);
+	}
+	
+	public function providerRenderableViewList() {
+		$human = new stdClass();
+		$human->name = 'Victor Cherubini';
+		$human->age = 25;
+		
+		return array(
+			array('welcome', array()),
+			array('replace-one', array('name' => 'Victor Cherubini')),
+			array('replace-two', array('name' => 'Victor Cherubini', 'age' => 25)),
+			array('list', array('list' => array('one', 'two', 'three'))),
+			array('object', array('human' => $human))
 		);
 	}
 	
