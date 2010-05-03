e40d83f87632060b480ddc51509fad9eaaedd6be
Vic Cherubini <vmc@leftnode.com>
2010-05-03 03:19:36 -0500

    Started to add a lot of the methods for rendering a view.

--- Jolt/View.php
+++ Jolt/View.php
@@ -7,4 +7,66 @@
  */
 class Jolt_View {
 	
+	const EXT = '.phtml';
+	
+	private $variable_list = array();
+	
+	
+	private $application_path = NULL; 
+	
+	private $block_directory = NULL;
+	
+	public function __construct() {
+		
+	}
+	
+	public function __destruct() {
+		
+		
+	}
+	
+	public function __get($k) {
+		return er($k, $this->variable_list, NULL);
+	}
+	
+	public function __set($k, $v) {
+		$this->variable_list[$k] = $v;
+		return true;
+	}
+	
+	
+	public function insertBlock($block_name) {
+		
+	}
+	
+	public function render($controller, $view) {
+		/**
+		 * 1. Determine the path to the controller file.
+		 * 2. Get all of the variables to replace.
+		 * 3. Require the file, and ob() it.
+		 * 4. Any calls to insertBlock() are processed.
+		 */
+		
+	}
+	
+	
+	public function getApplicationPath() {
+		return $this->application_path;
+	}
+	
+	public function getBlockDirectory() {
+		return $this->block_directory;
+	}
+	
+	
+	public function setApplicationPath($application_path) {
+		$this->application_path = rtrim($application_path, DIRECTORY_SEPARATOR);
+		return $this;
+	}
+	
+	public function setBlockDirectory($block_directory) {
+		$this->block_directory = rtrim($block_directory, DIRECTORY_SEPARATOR);
+		return $this;
+	}
+	
 }
