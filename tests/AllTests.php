990ef4f92ff7ea08b44ee2fab768bb14604f4266
Vic Cherubini <vmc@leftnode.com>
2010-05-03 04:22:18 -0500

    New define added, APPLICATION_DIRECTORY, which tells the test suite where to find the fake application data. Yes, all of this will be moved to a bootstrap file.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,6 +4,8 @@ $jolt_test_path = dirname(__FILE__);
 $jolt_path = $jolt_test_path . '/../';
 set_include_path(get_include_path() . PATH_SEPARATOR . $jolt_path . PATH_SEPARATOR . $jolt_test_path);
 
+define('APPLICATION_DIRECTORY', 'application', false);
+
 require_once 'PHPUnit/Framework.php';
 
 require_once 'TestCase.php';
