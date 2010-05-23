17dc554287be0cffc376cbfcdeacebc109edcbc9
Vic Cherubini <vmc@leftnode.com>
2010-05-23 15:07:46 -0500

    Added some doxygen comments.

--- Jolt/Layout.php
+++ Jolt/Layout.php
@@ -3,6 +3,11 @@
 declare(encoding='UTF-8');
 namespace Jolt;
 
+/**
+ * Defines a Layout view, which is the final result that's sent back to the
+ * client through \Jolt\Client. Thus, several \Jolt\View's can be combined
+ * and saved into a \Jolt\Layout.
+ */
 class Layout {
 	
 }
