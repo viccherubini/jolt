e1931419f7b08767782273a09fb6af23d18022ba
Vic Cherubini <vmc@leftnode.com>
2010-05-30 07:32:46 -0500

    Initial checkin of Session.php

---
+++ Jolt/Session.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace Jolt;
+
+/**
+ * Singleton for handling sessions easily. Provides a consistent and clean
+ * API for easy session handling.
+ *
+ * @author vmc <vmc@leftnode.com>
+ */
+class Session {
+
+	private function __construct() {
+		
+	}
+	
+	private function __clone() {
+		
+	}
+	
+	
+
+}
