7c04a2dcbc31e1a2d57fa9f57d41230361e18b52
Vic Cherubini <vmc@leftnode.com>
2010-06-01 06:04:42 -0500

    New test class for \Jolt\Client.

---
+++ tests/Client/ClientTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace Jolt;
+
+require_once 'Jolt/Client.php';
+
+class ClientTest extends TestCase {
+
+	public function testSettingHttpStatusOnlyAcceptsValidStatusNumbers() {
+		
+		
+	}
+}
