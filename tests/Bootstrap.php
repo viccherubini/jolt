72b2dd1ca783cd1562b879a6cbb703117e71df64
Vic Cherubini <vmc@leftnode.com>
2010-06-01 06:07:53 -0500

    Moved the includes of PHPUnit/Framework.php and TestCase.php here so any child testsuite can be executed with this boostrap file.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,6 +3,9 @@
 declare(encoding='UTF-8');
 namespace Jolt;
 
+require_once 'PHPUnit/Framework.php';
+require_once 'TestCase.php';
+
 $jolt_test_path = dirname(__FILE__);
 $jolt_path = $jolt_test_path . '/../';
 set_include_path(get_include_path() . PATH_SEPARATOR . $jolt_path . PATH_SEPARATOR . $jolt_test_path);
