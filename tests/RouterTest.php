9454ac4e2e7c76d2b78910d587b35bb1d2a8677b
Vic Cherubini <vmc@leftnode.com>
2010-07-26 15:45:27 -0500

    Added tests for setInputVariables().

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -9,15 +9,28 @@ require_once 'Jolt/Router.php';
 
 class RouterTest extends TestCase {
 
-	public function testRouteListIsInitiallyEmpty() {
+	public function testNewRouter_RouteListIsInitiallyEmpty() {
 		$router = new Router;
 		
 		$this->assertArray($router->getRouteList());
 		$this->assertEmptyArray($router->getRouteList());
 	}
 	
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testSetInputVariables_MustBeArray() {
+		$router = new Router;
+		$router->setInputVariables('11');
+	}
+	
+	public function testSetInputVariables_ReturnsRouterObject() {
+		$router = new Router;
+		$this->assertTrue($router->setInputVariables(array()) instanceof \Jolt\Router);
+	}
 	
 	
-	
-	
+	public function testExecute_FindsMatchedRoute() {
+		
+	}
 }
