9ab8cafff00490dc1ee8d8ab2ea9b2b2177a0a05
Vic Cherubini <vmc@leftnode.com>
2010-07-26 14:42:44 -0500

    Removed dispatcher tests in exchange for a temporary test until I can get around to writing the real Dispatcher class.

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -9,50 +9,8 @@ require_once 'Jolt/Dispatcher.php';
 
 class DispatcherTest extends TestCase {
 	
-	public function testControllerPathIsSetCorrectly() {
-		$dispatcher = new Dispatcher();
-		
-		$controller_path = getcwd();
-		$dispatcher->setControllerPath($controller_path);
-		
-		$this->assertEquals($controller_path, $dispatcher->getControllerPath());
-	}
-	
-	public function testApplicationPathIsSetCorrectly() {
-		$dispatcher = new Dispatcher();
-		
-		$application_path = getcwd();
-		$dispatcher->setApplicationPath($application_path);
-		
-		$this->assertEquals($application_path, $dispatcher->getApplicationPath());
-	}
-	
-	public function testLayoutPathIsSetCorrectly() {
-		$dispatcher = new Dispatcher();
-		
-		$layout_path = getcwd();
-		$dispatcher->setLayoutPath($layout_path);
-		
-		$this->assertEquals($layout_path, $dispatcher->getLayoutPath());
-	}
-	
-	
-	public function testRouteIsSetCorrectly() {
-		$dispatcher = new Dispatcher();
-		
-		$route = $this->buildMockNamedRoute('/user/%n', 'User', 'viewAction');
-		$dispatcher->setRoute($route);
-		
-		$this->assertTrue($route->isEqual($dispatcher->getRoute()));
-	}
-	
-	/**
-	 * @expectedException \Jolt\Exception
-	 */
-	public function testDispatcherMustHaveRouteBeforeExecuting() {
-		$dispatcher = new Dispatcher();
-		
-		$dispatcher->dispatch($this->buildMockClient());
+	public function testTrue() {
+		$this->assertTrue(true);
 	}
 	
 }
