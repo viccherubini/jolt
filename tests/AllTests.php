4767bd740f0fbc8472ce8bc9d72480d4d8448ffc
Vic Cherubini <vmc@leftnode.com>
2010-07-26 14:43:01 -0500

    Temporarily removed classes that aren't being tested yet.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -19,16 +19,16 @@ class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('Jolt All Tests');
 		
-		$suite->addTestSuite('\JoltTest\ClientTest');
-		$suite->addTestSuite('\JoltTest\ControllerTest');
-		$suite->addTestSuite('\JoltTest\DispatcherTest');
-		$suite->addTestSuite('\JoltTest\JoltTest');
+		//$suite->addTestSuite('\JoltTest\ClientTest');
+		//$suite->addTestSuite('\JoltTest\ControllerTest');
+		//$suite->addTestSuite('\JoltTest\DispatcherTest');
+		//$suite->addTestSuite('\JoltTest\JoltTest');
 		$suite->addTestSuite('\JoltTest\RegistryTest');
 		$suite->addTestSuite('\JoltTest\RouteTest');
 		$suite->addTestSuite(Route_AllTests::suite());
 		$suite->addTestSuite('\JoltTest\RouterTest');
 		$suite->addTestSuite('\JoltTest\TestCaseTest');
-		$suite->addTestSuite('\JoltTest\ViewTest');
+		//$suite->addTestSuite('\JoltTest\ViewTest');
 
 		return $suite;
 	}
