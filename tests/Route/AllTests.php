debe6b1d6fac9c5c13344a41a1b6c8ecf34ca8b5
Vic Cherubini <vmc@leftnode.com>
2010-07-26 15:20:41 -0500

    Added the NotFoundTest test class to the suite.

--- tests/Route/AllTests.php
+++ tests/Route/AllTests.php
@@ -6,6 +6,7 @@ namespace JoltTest;
 require_once 'PHPUnit/Framework.php';
 
 require_once 'Route/NamedTest.php';
+require_once 'Route/NotFoundTest.php';
 require_once 'Route/RestfulTest.php';
 
 class Route_AllTests {
@@ -14,6 +15,7 @@ class Route_AllTests {
 		$suite = new \PHPUnit_Framework_TestSuite('Jolt Route Tests');
 		
 		$suite->addTestSuite('\JoltTest\Route\NamedTest');
+		$suite->addTestSuite('\JoltTest\Route\NotFoundTest');
 		$suite->addTestSuite('\JoltTest\Route\RestfulTest');
 
 		return $suite;
