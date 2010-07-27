5024c93d9c9e027bff9a7bce53192cf356ee496e
Vic Cherubini <vmc@leftnode.com>
2010-07-26 21:46:13 -0500

    Updated the way set_include_path is made with only a single use of PATH_SEPARATOR, and used a realpath() around  to get the cannonical path since it's prettier.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,15 +1,18 @@
 <?php
 
 declare(encoding='UTF-8');
+namespace JoltTest;
 
 require_once 'PHPUnit/Framework.php';
 require_once 'TestCase.php';
 
+$includePath = get_include_path();
 $joltTestPath = dirname(__FILE__);
-$joltPath = $joltTestPath . '/../';
-set_include_path(get_include_path() . PATH_SEPARATOR . $joltPath . PATH_SEPARATOR . $joltTestPath);
+$joltPath = realpath($joltTestPath . '/../');
 
-define('TEST_DIRECTORY', $joltTestPath, false);
-define('APPLICATION_DIRECTORY', 'application', false);
+set_include_path(implode(PATH_SEPARATOR, array($includePath, $joltPath, $joltTestPath)));
+
+//define('TEST_DIRECTORY', $joltTestPath, false);
+//define('APPLICATION_DIRECTORY', 'application', false);
 
 require_once 'Jolt/Exception.php';
