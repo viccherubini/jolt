c82ca428568c90bb720f474c6b766178023a6480
Vic Cherubini <vmc@leftnode.com>
2010-07-28 06:49:11 -0500

    Made this class part of the \JoltTest\Route namespace, and included the new named specific tests.

--- tests/Route/AllTests.php
+++ tests/Route/AllTests.php
@@ -1,23 +1,24 @@
 <?php
 
 declare(encoding='UTF-8');
-namespace JoltTest;
+namespace JoltTest\Route;
+
+use \JoltTest\Route\Named\AllTests as NamedTests;
 
 require_once 'Jolt/Route.php';
 
-require_once 'PHPUnit/Framework.php';
-
+require_once 'Route/Named/AllTests.php';
 require_once 'Route/NamedTest.php';
-require_once 'Route/NotFoundTest.php';
 require_once 'Route/RestfulTest.php';
 
-class Route_AllTests {
+class AllTests {
 	
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('Jolt Route Tests');
+
+		$suite->addTestSuite(NamedTests::suite());
 		
 		$suite->addTestSuite('\JoltTest\Route\NamedTest');
-		$suite->addTestSuite('\JoltTest\Route\NotFoundTest');
 		$suite->addTestSuite('\JoltTest\Route\RestfulTest');
 
 		return $suite;
