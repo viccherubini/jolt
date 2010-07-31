d841ff1fd33d99119011fb0483f4427a559c0251
Vic Cherubini <vmc@leftnode.com>
2010-07-31 06:24:29 -0500

    Removed most of the controller code, not time to write it yet.

--- Jolt/Controller.php
+++ Jolt/Controller.php
@@ -5,11 +5,6 @@ namespace Jolt;
 
 abstract class Controller {
 	
-	private $layout = NULL;
-	
-	private $layout_object = NULL;
-	
-	private $config = array();
 	
 	public function __construct() {
 		
@@ -18,37 +13,5 @@ abstract class Controller {
 	public function __destruct() {
 		
 	}
-
-	public function render($view, $block=NULL) {
-		
-		
-	}
-	
-	
-	
-
-	public function getConfig() {
-		return $this->config;
-	}
-	
-	public function getLayout() {
-		return $this->layout;
-	}
-	
-	
-	
-	
-	public function setConfig(array $config) {
-		$this->config = $config;
-		return $this;
-	}
-	
-	
-	public function setLayout($layout) {
-		$this->layout = $layout;
-		return $this;
-	}
-	
-	
 	
 }
