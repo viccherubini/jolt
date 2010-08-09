6afcf1500e5e3cb2a791afaf7ba517550a19bf73
Vic Cherubini <vmc@leftnode.com>
2010-08-08 21:29:58 -0500

    New file that contains all of the essential Jolt framework filed.

---
+++ Jolt/Framework.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(encoding='UTF-8');
+
+require_once 'Jolt/Client.php';
+require_once 'Jolt/Configuration.php';
+require_once 'Jolt/Controller.php';
+require_once 'Jolt/Controller/Locator.php';
+require_once 'Jolt/Dispatcher.php';
+require_once 'Jolt/Exception.php';
+require_once 'Jolt/Router.php';
+require_once 'Jolt/View.php';
