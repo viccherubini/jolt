7d49aa6a5942919732cdcbcc1b74059394868dc7
Vic Cherubini <vmc@leftnode.com>
2010-08-12 06:12:37 -0500

    Got the main Jolt::execute() methods fixed and working.

--- Jolt/Framework.php
+++ Jolt/Framework.php
@@ -2,6 +2,8 @@
 
 declare(encoding='UTF-8');
 
+require_once 'Jolt/Jolt.php';
+
 require_once 'Jolt/Client.php';
 require_once 'Jolt/Configuration.php';
 require_once 'Jolt/Controller.php';
--- Jolt/Jolt.php
+++ Jolt/Jolt.php
@@ -14,17 +14,70 @@ require_once 'Exception.php';
  */
 class Jolt {
 	
-	private $configuration = NULL;
 	private $client = NULL;
+	private $configuration = NULL;
+	private $controllerLocator = NULL;
 	private $dispatcher = NULL;
 	private $router = NULL;
+	private $view = NULL;
 	
 	public function __construct() {
 
+	}
+	
+	public function attachClient(\Jolt\Client $client) {
+		$this->client = clone $client;
+		return $this;
 	}
 	
 	public function attachConfiguration(\Jolt\Configuration $cfg) {
 		$this->configuration = clone $cfg;
 		return $this;
 	}
+	
+	public function attachControllerLocator(\Jolt\Controller\Locator $locator) {
+		$this->controllerLocator = clone $locator;
+		return $this;
+	}
+	
+	public function attachDispatcher(\Jolt\Dispatcher $dispatcher) {
+		$this->dispatcher = clone $dispatcher;
+		return $this;
+	}
+	
+	public function attachRouter(\Jolt\Router $router) {
+		$this->router = clone $router;
+		return $this;
+	}
+	
+	public function attachView(\Jolt\View $view) {
+		$this->view = clone $view;
+		return $this;
+	}
+	
+	public function execute() {
+		$cfg = $this->configuration;
+		
+		$this->view->setViewPath($cfg->viewPath)
+			->setUrl($cfg->url)
+			->setSecureUrl($cfg->secureUrl)
+			->setUseRewrite($cfg->useRewrite);
+
+		$route = $this->router->execute();
+	
+		$this->dispatcher
+			->setControllerPath($cfg->controllerPath)
+			->attachLocator($this->controllerLocator)
+			->attachRoute($route)
+			->attachView($this->view)
+			->execute();
+
+		$this->client
+			->attachController($this->dispatcher->getController());
+		
+		return $this->client;
+	}
+	
+	
+	
 }
