0f80d5307e7748c7bee9731bd1d86752aa31b9b1
Vic Cherubini <vmc@leftnode.com>
2010-09-05 17:04:29 -0500

    Included most of the most common files to make Jolt work.

--- Jolt/Framework.php
+++ Jolt/Framework.php
@@ -2,6 +2,8 @@
 
 declare(encoding='UTF-8');
 
+require_once 'Jolt/Exception.php';
+
 require_once 'Jolt/Lib/Library.php';
 
 require_once 'Jolt/Client.php';
@@ -9,8 +11,13 @@ require_once 'Jolt/Configuration.php';
 require_once 'Jolt/Controller.php';
 require_once 'Jolt/Controller/Locator.php';
 require_once 'Jolt/Dispatcher.php';
-require_once 'Jolt/Exception.php';
+require_once 'Jolt/Registry.php';
+require_once 'Jolt/Route.php';
+require_once 'Jolt/Route/Named.php';
+require_once 'Jolt/Route/Named/Get.php';
+require_once 'Jolt/Route/Named/Post.php';
 require_once 'Jolt/Router.php';
+require_once 'Jolt/Session.php';
 require_once 'Jolt/View.php';
 
 require_once 'Jolt/Jolt.php';
