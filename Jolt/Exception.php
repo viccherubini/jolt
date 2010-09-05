7fae6eae2f57cf4f93ec5bf7a4c3ea564bb6d1ca
Vic Cherubini <vmc@leftnode.com>
2010-09-05 17:04:11 -0500

    Copied the Exception class from DataModeler to make it more useful in it's debug information.

--- Jolt/Exception.php
+++ Jolt/Exception.php
@@ -3,4 +3,32 @@
 declare(encoding='UTF-8');
 namespace Jolt;
 
-class Exception extends \Exception { }
+class Exception extends \Exception {
+
+	public function __construct($message=NULL) {
+		$msg = NULL;
+		
+		$trace = $this->getTrace();
+		$trace = current($trace);
+		
+		$filename = parent::getFile();
+		$lineNumber = parent::getLine();
+		
+		if ( isset($trace['class']) ) {
+			$msg = $trace['class'];
+		}
+		
+		if ( isset($trace['type']) ) {
+			$msg .= $trace['type'];
+		}
+		
+		if ( isset($trace['function']) ) {
+			$msg .= $trace['function'] . '()';
+		}
+
+		$msg .= " [{$message}] ({$filename} +{$lineNumber})";
+
+		parent::__construct($msg);
+	}
+
+}
