1eeb336a1e5b59f9f6cec725085f1c531cf3c704
Vic Cherubini <vmc@leftnode.com>
2010-09-23 06:11:25 -0500

    In a daring, and one might say blazened checkin, I added all of the code to save session data in a database through PDO. Get this, it's even entirely untested. Crazy, I know, but I needed the functionality and didn't have time to do correct TDD, so it's there. It appears to work, but I can't prove it yet. Rest assured, it will ultimately be tested. Just not now.

--- Jolt/Session.php
+++ Jolt/Session.php
@@ -12,17 +12,23 @@ namespace Jolt;
 class Session {
 
 	private static $instance = NULL;
+	
+	private $pdo = NULL;
+	private $saveHandler = NULL;
 	private $sessionName = NULL;
 	private $sessionId = NULL;
 
-	private $started = false;
+	private $ip = NULL;
+	private $agent = NULL;
 
+	private $started = false;
+	
 	private function __construct() {
-		
+	
 	}
 	
 	private function __clone() {
-		
+	
 	}
 	
 	public function __set($k, $v) {
@@ -55,7 +61,22 @@ class Session {
 		return self::$instance;
 	}
 	
-	public function start($sessionName = NULL) {
+	public function setIp($ip) {
+		$this->ip = trim($ip);
+		return $this;
+	}
+	
+	public function setAgent($agent) {
+		$this->agent = trim($agent);
+		return $this;
+	}
+	
+	public function attachPdo(\PDO $pdo) {
+		$this->pdo = $pdo;
+		return $this;
+	}
+	
+	public function start($sessionName=NULL) {
 		$started = false;
 		
 		if ( php_sapi_name() != 'cli' ) {
@@ -66,7 +87,21 @@ class Session {
 						$sessionName = NULL;
 					}
 				}
+				
+				if ( $this->pdo instanceof \PDO ) {
+					ini_set('session.save_handler', 'user');
+					register_shutdown_function('session_write_close');
 
+					session_set_save_handler(
+						array($this, 'open'),
+						array($this, 'close'),
+						array($this, 'read'),
+						array($this, 'write'),
+						array($this, 'destroy'),
+						array($this, 'gc')
+					);
+				}
+				
 				if ( !empty($sessionName) ) {
 					$this->sessionName = $sessionName;
 					session_name($sessionName);
@@ -92,6 +127,120 @@ class Session {
 		}
 
 		session_destroy();
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Request handlers for saving session data to the database.
+	 */
+
+	public function open() {
+		return true;
+	}
+	
+	public function close() {
+		$maxLifetime = (int)get_cfg_var('session.gc_maxlifetime');
+		$this->gc($maxLifetime);
+		
+		return true;
+	}
+	
+	public function read($sessionId) {
+		$data = NULL;
+		
+		try {
+			$pdoStatement = $this->pdo
+				->prepare('SELECT data FROM session WHERE session_id = :session_id');
+			$pdoStatement->execute(array('session_id' => $sessionId));
+			$session = $pdoStatement->fetchObject();
+			
+			if ( is_object($session) && property_exists($session, 'data') ) {
+				$data = $session->data;
+			}
+		} catch ( \Exception $e ) { }
+		
+		return $data;
+	}
+	
+	public function write($sessionId, $data) {
+		$expiration = time();
+		$now = date('Y-m-d H:i:s');
+		
+		try {
+			// Find the session first
+			$pdoStatement = $this->pdo
+				->prepare('SELECT COUNT(*) AS session_count FROM session WHERE session_id = :session_id');
+			$pdoStatement->execute(array('session_id' => $sessionId));
+			$session = $pdoStatement->fetch(\PDO::FETCH_OBJ);
+			
+			$sessionCount = 0;
+			if ( property_exists($session, 'session_count') ) {
+				$sessionCount = (int)$session->session_count;
+			}
+			
+			if ( 1 == $sessionCount ) {
+				$parameters = array(
+					'updated' => $now,
+					'expiration' => $expiration,
+					'data' => $data,
+					'session_id' => $sessionId
+				);
+				
+				$sql = 'UPDATE session SET
+						updated = :updated, expiration = :expiration, data = :data
+					WHERE session_id = :session_id';
+				
+				$pdoStatement = $this->pdo
+					->prepare($sql);
+				
+				$pdoStatement->execute($parameters);
+			} elseif ( 0 == $sessionCount ) {
+				$parameters = array(
+					'session_id' => $sessionId,
+					'created' => $now,
+					'updated' => '',
+					'expiration' => $expiration,
+					'ip' => $this->ip,
+					'agent' => $this->agent,
+					'agent_hash' => sha1($this->agent),
+					'data' => $data
+				);
+				
+				$sql = 'INSERT INTO session ( session_id, created, updated, expiration, ip, agent, agent_hash, data )
+					VALUES ( :session_id, :created, :updated, :expiration, :ip, :agent, :agent_hash, :data)';
+				
+				$pdoStatement = $this->pdo
+					->prepare($sql);
+					
+				$pdoStatement->execute($parameters);
+			} else {
+				//$sql = 'DELETE FROM se
+			}
+		} catch ( \Exception $e ) { }
+		
+		return true;
+	}
+	
+	public function destroy($sessionId) {
+		try {
+			$pdoStatement = $this->pdo
+				->prepare('DELETE FROM session WHERE session_id = :session_id');
+			$pdoStatement->execute(array('session_id' => $sessionId));
+		} catch ( \Exception $e ) { }
+		
+		return true;
+	}
+	
+	public function gc($lifetime) {
+		try {
+			$expiration = time() - $lifetime;
+			
+			$pdoStatement = $this->pdo
+				->prepare('DELETE FROM session WHERE expiration < :expiration');
+			$pdoStatement->execute(array('expiration' => $expiration));
+		} catch ( \Exception $e ) { }
 		
 		return true;
 	}
