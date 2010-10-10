47b75091b2341f400e290d220c9e513f1f6722fc
Vic Cherubini <vmc@leftnode.com>
2010-10-10 12:59:35 -0500

    Made the class work better with the users code.
    * Can now specify what types of exceptions should be thrown on an error
    * Can pass entire $_POST array in and define a key to fetch from there
    * Exceptions are thrown throw throwException() now

--- Jolt/Form.php
+++ Jolt/Form.php
@@ -5,12 +5,13 @@ namespace Jolt;
 
 class Form {
 
-	private $id = NULL;
+	private $dataKey = NULL;
+	private $exception = NULL;
 	
 	private $data = array();
 	private $validator = array();
 
-	private $errorList = array();
+	private $messages = array();
 
 	public function __construct() {
 	
@@ -20,10 +21,14 @@ class Form {
 		$this->data = array();
 	}
 	
+	public function addMessage($message) {
+		$this->messages[] = $message;
+		return $this;
+	}
 	
 	public function validate() {
-		
-		if ( 0 == count($this->data) ) {
+		$data = $this->getData();
+		if ( 0 == count($data) ) {
 			return true;
 		}
 		
@@ -35,21 +40,37 @@ class Form {
 			return true;
 		}
 		
+		$invalid = false;
 		foreach ( $data as $dataKey => $dataValue ) {
-			$ruleSet = $validator[$dataKey];
-			
+			$ruleSet = $this->validator[$dataKey];
 			if ( !$ruleSet->isValid($dataValue) ) {
-				
+				$this->addMessage($ruleSet->getMessage());
+				$invalid = true;
 			}
 		}
 		
-		return true;
+		if ( $invalid ) {
+			$this->throwException('The form failed to validate.');
+		}
+		
+		//return (!$invalid);
 	}
+	
 	
 	
 	public function setData(array $data) {
 		ksort($data);
 		$this->data = $data;
+		return $this;
+	}
+	
+	public function setDataKey($dataKey) {
+		$this->dataKey = trim($dataKey);
+		return $this;
+	}
+
+	public function setException($exception) {
+		$this->exception = trim($exception);
 		return $this;
 	}
 
@@ -60,31 +81,40 @@ class Form {
 		$this->validator = $validator;
 		return $this;
 	}
-
-	public function getErrorList() {
-		return $this->errorList;
+	
+	public function getData() {
+		if ( !empty($this->dataKey) && array_key_exists($this->dataKey, $this->data) ) {
+			return $this->data[$this->dataKey];
+		}
+		return $this->data;
 	}
 
-	
+	public function getMessages() {
+		return $this->messages;
+	}
+
+
 	private function checkValidatorElementsAreRuleSets($validator) {
 		foreach ( $validator as $ruleKey => $ruleSet ) {
 			if ( !($ruleSet instanceof \Jolt\Form\RuleSet) ) {
-				throw new \Jolt\Exception("The element '{$ruleKey}' is not of type \Jolt\Form\RuleSet.");
+				$this->throwException("The element '{$ruleKey}' is not of type \Jolt\Form\RuleSet.");
 			}
 		}
 		return true;
 	}
 
 	private function checkDataAndValidatorCounts() {
-		if ( count($this->data) !== count($this->validator) ) {
-			throw new \Jolt\Exception('The number of elements in the data do not match the number of elements in the validator.');
+		$data = $this->getData();
+		if ( count($data) !== count($this->validator) ) {
+			$this->throwException('The number of elements in the data do not match the number of elements in the validator.');
 		}
 		return true;
 	}
 	
 	private function checkDataAndValidatorElements() {
-		if ( array_keys($this->data) !== array_keys($this->validator) ) {
-			throw new \Jolt\Exception('The elements of the data do not match the elements of the validator.');
+		$data = $this->getData();
+		if ( array_keys($data) !== array_keys($this->validator) ) {
+			$this->throwException('The elements of the data do not match the elements of the validator.');
 		}
 		return true;
 	}
@@ -92,10 +122,18 @@ class Form {
 	private function checkEmptyRuleSets() {
 		$allEmpty = true;
 		foreach ( $this->validator as $ruleSet ) {
-			if ( $ruleSet->isEmpty() ) {
+			if ( !$ruleSet->isEmpty() ) {
 				$allEmpty = false;
 			}
 		}
 		return $allEmpty;
+	}
+	
+	private function throwException($message) {
+		$exception = $this->exception;
+		if ( empty($exception) || !class_exists($exception) ) {
+			$exception = '\Jolt\Exception';
+		}
+		throw new $exception($message);
 	}
 }
