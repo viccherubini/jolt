9172897147c892567ad831fa2e3ccb9cd88399fe
Vic Cherubini <vmc@leftnode.com>
2010-10-13 05:53:34 -0500

    Starting to write the basic class structure for the \Jolt\Form\Validator class.

---
+++ Jolt/Form/Validator.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(encoding='UTF-8');
+namespace Jolt\Form;
+
+class Validator {
+	
+	public function __construct($ruleSets=array()) {
+		
+	}
+	
+	
+	public function addRuleSet($field, \Jolt\Validator\RuleSet $ruleSet) {
+		
+	}
+	
+	
+	
+}
