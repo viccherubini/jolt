d9326413a35badf81718c0d5c4f8dd0250a21bfc
Vic Cherubini <vmc@leftnode.com>
2010-10-14 05:49:00 -0500

    Wrote all of the tests for each of the different operations.

--- Jolt/Form/Validator/RuleSet.php
+++ Jolt/Form/Validator/RuleSet.php
@@ -9,17 +9,39 @@ class RuleSet {
 
 	private $field = NULL;
 
-	private $rules = array();
+	private $errors = array();
 	private $messages = array();
+	private $rules = array();
 	
 	public function __construct(array $rules, array $messages=array(), $field=NULL) {
-		$this->rules = $rules;
-		$this->messages = $messages;
+		foreach ( $rules as $ruleKey => $ruleValue ) {
+			$this->addRule($ruleKey, $ruleValue);
+			if ( array_key_exists($ruleKey, $messages) ) {
+				$this->addMessage($ruleKey, $messages[$ruleKey]);
+			}
+		}
+		
 		$this->field = $field;
 	}
 	
 	public function __destruct() {
 		$this->rules = array();
+	}
+	
+	public function addRule($ruleKey, $rule) {
+		if ( empty($ruleKey) ) {
+			throw new \Jolt\Exception('the rule can not be empty');
+		}
+		$this->rules[$ruleKey] = $rule;
+		return $this;
+	}
+	
+	public function addMessage($ruleKey, $message) {
+		if ( empty($ruleKey) ) {
+			throw new \Jolt\Exception('the rules message can not be empty');
+		}
+		$this->messages[$ruleKey] = $message;
+		return $this;
 	}
 	
 	public function isEmpty() {
@@ -29,7 +51,7 @@ class RuleSet {
 	public function isValid($value) {
 		$isValid = true;
 		foreach ( $this->rules as $op => $rule ) {
-			$opMethod = 'op_' . strtolower($op);
+			$opMethod = 'op' . ucwords(strtolower($op));
 			if ( method_exists($this, $opMethod) && !$this->$opMethod($rule, $value) ) {
 				$isValid = false;
 				break;
@@ -38,72 +60,91 @@ class RuleSet {
 		return $isValid;
 	}
 	
-	private function op_empty($empty, $value) {
+	public function getField() {
+		return $this->field;
+	}
+	
+	public function getMessages() {
+		return $this->messages;
+	}
+	
+	
+	private function opEmpty($empty, $value) {
 		if ( empty($value) ) {
-			//$this->setMessage(sprintf('The field %s can not be empty.', $this->field));
 			return false;
 		}
 		return true;
 	}
 	
-	private function op_minlength($minlength, $value) {
+	private function opMinlength($minlength, $value) {
 		$minlength = (int)$minlength;
 		if ( mb_strlen($value, $this->charset) < $minlength ) {
-			//$this->setMessage(sprintf('The field %s must have a length greater than or equal to %d characters.', $this->field, $minlength));
 			return false;
 		}
 		return true;
 	}
 	
-	private function op_maxlength($maxlength, $value) {
+	private function opMaxlength($maxlength, $value) {
 		$maxlength = (int)$maxlength;
 		if ( mb_strlen($value, $this->charset) > $maxlength ) {
-			//$this->setMessage(sprintf('The field %s must have a length less than or equal to %d characters.', $this->field, $maxlength));
 			return false;
 		}
 		return true;
 	}
 	
-	private function op_nonzero($nonzero, $value) {
-		$value = (int)$value;
-		if ( 0 === $value ) {
-			//$this->setMessage(sprintf('The field %s can not have a value of 0.', $this->field));
-			return false;
-		}
-		return true;
-	}
-	
-	private function op_numeric($numeric, $value) {
+	private function opNonzero($nonzero, $value) {
 		if ( !is_numeric($value) ) {
-			//$this->setMessage(sprintf('The field %s must be numeric', $this->field));
+			$value = (int)$value;
+		}
+		
+		if ( 0 === $value ) {
 			return false;
 		}
 		return true;
 	}
 	
-	private function op_email($email, $value) {
-		return true;
-	}
-	
-	private function op_inarray($inarray, $value) {
-		if ( !in_array($value, $inarray) ) {
-			//$this->setMessage(sprintf('An invalid value was specified for the field %s.', $this->field));
+	private function opNumeric($numeric, $value) {
+		if ( !is_numeric($value) ) {
 			return false;
 		}
 		return true;
 	}
 	
-	private function op_regex($regex, $value) {
+	private function opEmail($email, $value) {
+		// We're not very strict about email addresses. Essentially: anything@anything
+		$value = trim($value);
+		if ( false === stripos($value, '@') ) {
+			return false;
+		}
+		
+		// Must have something on left hand side and something on right hand side
+		$bits = explode('@', $value);
+		
+		// Count must be exactly 2, can't have multiple @ signs
+		if ( 2 !== count($bits) ) {
+			return false;
+		}
+		
+		// Length of each side must be greater than 0
+		return ( mb_strlen($bits[0], $this->charset) > 0 && mb_strlen($bits[1], $this->charset) ); 
+	}
+	
+	private function opInarray($inarray, $value) {
+		if ( !in_array($value, $inarray, true) ) {
+			return false;
+		}
+		return true;
+	}
+	
+	private function opRegex($regex, $value) {
 		if ( 1 !== preg_match($regex, $value) ) {
-			//$this->setMessage(sprintf('An invalid value was specified for the field %s.', $this->field));
 			return false;
 		}
 		return true;
 	}
 	
-	private function op_callback($callback, $value) {
+	private function opCallback($callback, $value) {
 		if ( !$callback($value) ) {
-			//$this->setMessage(sprintf('An invalid value was specified for the field %s.', $this->field));
 			return false;
 		}
 		return true;
