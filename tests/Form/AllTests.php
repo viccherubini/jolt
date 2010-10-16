74ab3a83573793a21c4fbbc1ee14bcd779245dd2
Vic Cherubini <vmc@leftnode.com>
2010-10-16 07:22:34 -0500

    Separated the two different test suites out from the root level form test suites and and the in depth test suites. Also included the WriterTest test suite to make sure that is covered as well.

--- tests/Form/AllTests.php
+++ tests/Form/AllTests.php
@@ -4,15 +4,17 @@ declare(encoding='UTF-8');
 namespace JoltTest\Form;
 
 require_once 'Form/ValidatorTest.php';
+require_once 'Form/WriterTest.php';
+
 require_once 'Form/RuleSet/RuleSetTest.php';
 require_once 'Form/Writer/DbTest.php';
 require_once 'Form/Writer/SessionTest.php';
 
 class AllTests {
-	
+
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('Jolt Form Tests');
-		
+
 		$suite->addTestSuite('\JoltTest\Form\ValidatorTest');
 		$suite->addTestSuite('\JoltTest\Form\Validator\RuleSetTest');
 		$suite->addTestSuite('\JoltTest\Form\WriterTest');
