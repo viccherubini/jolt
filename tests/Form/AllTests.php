c41496dc12125be6cf7830f7bdf1801618f85424
Vic Cherubini <vmc@leftnode.com>
2010-10-15 21:03:45 -0500

    Added the WriterTest, Writer\DbTest and Writer\SessionTest test classes to the Form test suite.

--- tests/Form/AllTests.php
+++ tests/Form/AllTests.php
@@ -5,6 +5,8 @@ namespace JoltTest\Form;
 
 require_once 'Form/ValidatorTest.php';
 require_once 'Form/RuleSet/RuleSetTest.php';
+require_once 'Form/Writer/DbTest.php';
+require_once 'Form/Writer/SessionTest.php';
 
 class AllTests {
 	
@@ -13,6 +15,9 @@ class AllTests {
 		
 		$suite->addTestSuite('\JoltTest\Form\ValidatorTest');
 		$suite->addTestSuite('\JoltTest\Form\Validator\RuleSetTest');
+		$suite->addTestSuite('\JoltTest\Form\WriterTest');
+		$suite->addTestSuite('\JoltTest\Form\Writer\DbTest');
+		$suite->addTestSuite('\JoltTest\Form\Writer\SessionTest');
 
 		return $suite;
 	}
