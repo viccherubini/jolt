44297c8a7a8b489bd34eeca7a48fa41e94092bcf
Vic Cherubini <vmc@leftnode.com>
2010-10-16 07:20:31 -0500

    Wrote the write() method to save the form data as serialized JSON to a datastore. It's not the prettiest thing, but it's very fast and only requires one table. Additionally, added the ability to change the name of the table that the data is stored to. It is 'form' by default, but the user could easily change it to something else.

--- Jolt/Form/Writer/Db.php
+++ Jolt/Form/Writer/Db.php
@@ -7,6 +7,7 @@ use \Jolt\Form\Writer;
 class Db extends Writer {
 
 	private $pdo = NULL;
+	private $table = 'form';
 
 	public function attachPdo(\PDO $pdo) {
 		$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
@@ -17,11 +18,64 @@ class Db extends Writer {
 	}
 
 	public function write() {
+		$pdo = $this->getPdo();
+		if ( is_null($pdo) ) {
+			return false;
+		}
 
+		$data = $this->getData();
+		if ( empty($data) || 0 === count($data) ) {
+			return false;
+		}
+
+		$id = $this->getId();
+		if ( empty($id) ) {
+			return false;
+		}
+
+		$created = date('Y-m-d H:i:s', time());
+		$dataKey = $this->getDataKey();
+		$name = $this->getName();
+		$table = $this->getTable();
+
+		$dataJson = json_encode($data);
+
+		$sql = "INSERT INTO {$table} (created, id, name, datakey, data, status) VALUES(:created, :id, :name, :datakey, :data, :status)";
+
+		$pdo->beginTransaction();
+			$stmt = $pdo->prepare($sql);
+
+			if ( !$stmt ) {
+				$pdo->rollback();
+				return false;
+			}
+
+			$parameters = array(
+				'created' => $created,
+				'id' => $id,
+				'name' => $name,
+				'datakey' => $dataKey,
+				'data' => $dataJson,
+				'status' => 1
+			);
+
+			$executed = $stmt->execute($parameters);
+		$pdo->commit();
+
+		return $executed;
 	}
 
+	public function setTable($table) {
+		$this->table = trim($table);
+		return $this;
+	}
 
 	public function getPdo() {
 		return $this->pdo;
 	}
+
+	public function getTable() {
+		return $this->table;
+	}
+
 }
