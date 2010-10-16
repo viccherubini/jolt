41e924cc61d1cd1ef4616cc0c089f04479b1415e
Vic Cherubini <vmc@leftnode.com>
2010-10-16 07:24:07 -0500

    The new directory 'db' holds all of the scripts necessary to get the test suite to run. Generally they're divided by the database type. The DbTest suite provides 100% test coverage for the \Jolt\Form\Writer\Db class.

--- tests/Form/Writer/DbTest.php
+++ tests/Form/Writer/DbTest.php
@@ -11,6 +11,27 @@ require_once 'Jolt/Form/Writer/Db.php';
 
 class DbTest extends TestCase {
 
+	private $pdo = NULL;
+	private $formDataKey = NULL;
+
+	private $formData = array();
+
+	public function setUp() {
+		$sql = file_get_contents(DIRECTORY_DB . DS . 'sqlite.sql');
+
+		$this->pdo = $this->getMockForAbstractClass('\PDO', array('sqlite::memory:'));
+		$this->pdo->exec($sql);
+
+		$this->formDataKey = 'article';
+		$this->formData = array(
+			'article' => array(
+				'title' => 'The title of the "article" goes here',
+				'content' => "Article's are just about the 'coolest!' thing you can wrote content is here",
+				'created' => date('Y-m-d H:i:s', time())
+			)
+		);
+	}
+
 	/**
 	 * @expectedException PHPUnit_Framework_Error
 	 */
@@ -19,13 +40,61 @@ class DbTest extends TestCase {
 		$db->attachPdo(NULL);
 	}
 
-	public function testAttachPdo_SilentErrors() {
-		$pdo = $this->getMockForAbstractClass('\PDO', array('sqlite:memory:'));
-
+	public function testWrite_FailsForNoPdo() {
 		$db = new Db;
-		$db->attachPdo($pdo);
-
-		$pdo = $db->getPdo();
+		$this->assertFalse($db->write());
 	}
 
+	public function testWrite_FailsForNoData() {
+		$db = new Db;
+		$db->attachPdo($this->pdo);
+
+		$this->assertFalse($db->write());
+	}
+
+	public function testWrite_FailsForNoId() {
+		$db = new Db;
+		$db->attachPdo($this->pdo);
+		$db->setData(array('name' => 'Vic Cherubini', 'age' => 26));
+
+		$this->assertFalse($db->write());
+	}
+
+	public function testWrite_FailsForMissingTable() {
+		$db = $this->buildDbWriterWithPdoAttached('article');
+		$db->setTable('missing_table');
+		$written = $db->write();
+
+		$this->assertFalse($written);
+	}
+
+	public function testWrite_WritesFormData() {
+		$db = $this->buildDbWriterWithPdoAttached('article');
+		$written = $db->write();
+
+		$this->assertTrue($written);
+	}
+
+	public function testWrite_WritesToDifferentTable() {
+		$db = $this->buildDbWriterWithPdoAttached('article');
+		$db->setTable('form_alternative');
+		$written = $db->write();
+
+		$this->assertTrue($written);
+	}
+
+
+
+
+	private function buildDbWriterWithPdoAttached($name) {
+		$db = new Db;
+
+		$db->attachPdo($this->pdo);
+		$db->setId(uniqid('jolt.', true));
+		$db->setName($name);
+		$db->setDataKey($this->formDataKey);
+		$db->setData($this->formData);
+
+		return $db;
+	}
 }
