3c148e4c3620817d9ae05cbf93fd72a015d4321f
Vic Cherubini <vmc@leftnode.com>
2010-10-17 06:11:21 -0500

    Removed all of the centrally defined methods to these classes and moved them to \Jolt\Controller. They now consist of the abstract methods they need defined. The reason these classes are kept and not made into an interface is because I still want to use type hinting to be able to determine the difference between a Writer sub class and a Loader sub class.

--- Jolt/Form/Loader.php
+++ Jolt/Form/Loader.php
@@ -2,63 +2,12 @@
 
 declare(encoding='UTF-8');
 namespace Jolt\Form;
+use \Jolt\Form\Controller;
 
-abstract class Loader {
+require_once 'Jolt/Form/Controller.php';
 
-	private $id = NULL;
-	private $dataKey = NULL;
-	private $name = NULL;
-
-	private $data = array();
-
-	public function __construct() {
-
-	}
-
-	public function __destruct() {
-		$this->reset();
-	}
-
-	public function reset() {
-		$this->data = array();
-		$this->dataKey = NULL;
-	}
-
-	public function setId($id) {
-		$this->id = trim($id);
-		return $this;
-	}
-
-	public function setName($name) {
-		$this->name = trim($name);
-		return $this;
-	}
-
-	public function setData(array $data) {
-		$this->data = $data;
-		return $this;
-	}
-
-	public function setDataKey($dataKey) {
-		$this->dataKey = trim($dataKey);
-		return $this;
-	}
-
-	public function getId() {
-		return $this->id;
-	}
-
-	public function getName() {
-		return $this->name;
-	}
-
-	public function getData() {
-		return $this->data;
-	}
-
-	public function getDataKey() {
-		return $this->dataKey;
-	}
+abstract class Loader extends Controller {
 
 	abstract public function load();
+
 }
--- Jolt/Form/Writer.php
+++ Jolt/Form/Writer.php
@@ -2,63 +2,12 @@
 
 declare(encoding='UTF-8');
 namespace Jolt\Form;
+use \Jolt\Form\Controller;
 
-abstract class Writer {
-	
-	private $id = NULL;
-	private $dataKey = NULL;
-	private $name = NULL;
+require_once 'Jolt/Form/Controller.php';
 
-	private $data = array();
-	
-	public function __construct() {
-		
-	}
-	
-	public function __destruct() {
-		$this->reset();
-	}
-	
-	public function reset() {
-		$this->data = array();
-		$this->dataKey = NULL;
-	}
-	
-	public function setId($id) {
-		$this->id = trim($id);
-		return $this;
-	}
-	
-	public function setName($name) {
-		$this->name = trim($name);
-		return $this;
-	}
-	
-	public function setData(array $data) {
-		$this->data = $data;
-		return $this;
-	}
-	
-	public function setDataKey($dataKey) {
-		$this->dataKey = trim($dataKey);
-		return $this;
-	}
-	
-	public function getId() {
-		return $this->id;
-	}
-	
-	public function getName() {
-		return $this->name;
-	}
-	
-	public function getData() {
-		return $this->data;
-	}
-	
-	public function getDataKey() {
-		return $this->dataKey;
-	}
-	
+abstract class Writer extends Controller {
+
 	abstract public function write();
+
 }
