de59a32a37eb8789c985d59eb9562cf3ccf992ee
Vic Cherubini <vmc@leftnode.com>
2010-10-17 04:58:58 -0500

    Added a tearDown() method to explicitly invoke the destructor of the Writer class.

--- tests/Form/WriterTest.php
+++ tests/Form/WriterTest.php
@@ -11,21 +11,25 @@ require_once 'Jolt/Form/Writer.php';
 class WriterTest extends TestCase {
 
 	private $writer = NULL;
-	
+
 	public function setUp() {
 		$this->writer = $this->getMockForAbstractClass('\Jolt\Form\Writer');
 	}
 
+	public function tearDown() {
+		$this->writer = NULL;
+	}
+
 	public function testReset_DataIsEmpty() {
 		$data = array('a' => 'b', 'c' => 'd');
-		
+
 		$this->writer->setData($data);
 		$this->writer->setDataKey('a');
-		
+
 		$this->assertGreaterThan(1, count($this->writer->getData()));
-		
+
 		$this->writer->reset();
-		
+
 		$this->assertEmpty($this->writer->getData());
 		$this->assertNull($this->writer->getDataKey());
 	}
@@ -33,19 +37,19 @@ class WriterTest extends TestCase {
 	public function testSetId_IsTrimmed() {
 		$id = '   id   ';
 		$idTrimmed = 'id';
-		
+
 		$this->writer->setId($id);
 		$this->assertEquals($idTrimmed, $this->writer->getId());
 	}
-	
+
 	public function testSetName_IsTrimmed() {
 		$name = '   name   ';
 		$nameTrimmed = 'name';
-		
+
 		$this->writer->setName($name);
 		$this->assertEquals($nameTrimmed, $this->writer->getName());
 	}
-	
+
 	/**
 	 * @expectedException PHPUnit_Framework_Error
 	 */
