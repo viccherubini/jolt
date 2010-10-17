dd7991a4858e58256341b84897d92330c67730a5
Vic Cherubini <vmc@leftnode.com>
2010-10-17 06:13:32 -0500

    Removed the separate Writer and Loader classes from AllTests and added the new ControllerTest class.

--- tests/Form/AllTests.php
+++ tests/Form/AllTests.php
@@ -3,9 +3,8 @@
 declare(encoding='UTF-8');
 namespace JoltTest\Form;
 
+require_once 'Form/ControllerTest.php';
 require_once 'Form/ValidatorTest.php';
-require_once 'Form/LoaderTest.php';
-require_once 'Form/WriterTest.php';
 
 require_once 'Form/RuleSet/RuleSetTest.php';
 require_once 'Form/Writer/DbTest.php';
@@ -16,12 +15,12 @@ class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('Jolt Form Tests');
 
+		$suite->addTestSuite('\JoltTest\Form\ControllerTest');
 		$suite->addTestSuite('\JoltTest\Form\ValidatorTest');
+
 		$suite->addTestSuite('\JoltTest\Form\Validator\RuleSetTest');
-		$suite->addTestSuite('\JoltTest\Form\LoaderTest');
-		$suite->addTestSuite('\JoltTest\Form\WriterTest');
+		$suite->addTestSuite('\JoltTest\Form\Loader\DbTest');
 		$suite->addTestSuite('\JoltTest\Form\Writer\DbTest');
-		$suite->addTestSuite('\JoltTest\Form\Writer\SessionTest');
 
 		return $suite;
 	}
