8e93f543b24cb91eb4ea385bf97f8848dc20a223
Vic Cherubini <vmc@leftnode.com>
2010-10-17 04:40:25 -0500

    Changed the save() method to write() to reflect the method in the \Jolt\Form\Writer class.

--- Jolt/Form.php
+++ Jolt/Form.php
@@ -8,83 +8,83 @@ class Form {
 	private $id = NULL;
 	private $name = NULL;
 	private $dataKey = NULL;
-	
+
 	private $exception = NULL;
 	private $loader = NULL;
 	private $validator = NULL;
 	private $writer = NULL;
-	
+
 	private $data = array();
-	
+
 	private $messages = array();
 
 	public function __construct() {
-	
+
 	}
-	
+
 	public function __destruct() {
 		$this->data = array();
 	}
-	
+
 	public function addMessage($field, $message) {
 		$this->messages[$field] = $message;
 		return $this;
 	}
-	
+
 	public function attachException(\Exception $exception) {
 		$this->exception = $exception;
 		return $this;
 	}
-	
+
 	public function attachLoader(\Jolt\Form\Loader $loader) {
 		$this->loader = $loader;
 		return $this;
 	}
-	
+
 	public function attachWriter(\Jolt\Form\Writer $writer) {
 		$this->writer = $writer;
 		return $this;
 	}
-	
+
 	public function attachValidator(\Jolt\Form\Validator $validator) {
 		$this->validator = $validator;
 		return $this;
 	}
-	
+
 	public function load() {
-		
+
 	}
-	
-	public function save() {
-		
+
+	public function write() {
+
 	}
-	
+
 	public function validate() {
-		
+
 	}
-	
+
 	public function message($field) {
 		if ( array_key_exists($field, $this->messages) ) {
 			return $this->messages[$field];
 		}
 		return NULL;
 	}
-	
+
 	public function setId($id) {
 		$this->id = trim($id);
 		return $this;
 	}
-	
+
 	public function setName($name) {
 		$this->name = trim($name);
 		return $this;
 	}
-	
+
 	public function setData(array $data) {
 		$this->data = $data;
 		return $this;
 	}
-	
+
 	public function setDataKey($dataKey) {
 		$this->dataKey = trim($dataKey);
 		return $this;
@@ -93,36 +93,36 @@ class Form {
 	public function getId() {
 		return $this->id;
 	}
-	
+
 	public function getName() {
 		return $this->name;
 	}
-	
+
 	public function getData() {
 		return $this->data;
 	}
-	
+
 	public function getDataKey() {
 		return $this->dataKey;
 	}
-	
+
 	public function getException() {
 		return $this->exception;
 	}
-	
+
 	public function getDataSet() {
 		if ( !empty($this->dataKey) && array_key_exists($this->dataKey, $this->data) ) {
 			return $this->data[$this->dataKey];
 		}
 		return $this->data;
 	}
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 
 
 	private function checkDataAndValidatorCounts() {
@@ -132,7 +132,7 @@ class Form {
 		}
 		return true;
 	}
-	
+
 	private function checkDataAndValidatorElements() {
 		$data = $this->getData();
 		if ( array_keys($data) !== array_keys($this->validator) ) {
@@ -140,7 +140,7 @@ class Form {
 		}
 		return true;
 	}
-	
+
 	private function checkEmptyRuleSets() {
 		$allEmpty = true;
 		foreach ( $this->validator as $ruleSet ) {
@@ -150,5 +150,5 @@ class Form {
 		}
 		return $allEmpty;
 	}
-	
+
 }
