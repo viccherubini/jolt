833c5087241bd177afdea840b2a22c7a493782cb
Vic Cherubini <vmc@leftnode.com>
2010-10-17 09:13:03 -0500

    Added new tests for the attach*() methods in \Jolt\Form.

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -7,48 +7,104 @@ use \Jolt\Form,
 	\JoltTest\TestCase;
 
 require_once 'Jolt/Form.php';
+require_once 'Jolt/Form/Loader.php';
+require_once 'Jolt/Form/Writer.php';
 
 class FormTest extends TestCase {
-	
+
 	public function testAddMessage_OverwritesExistingMessages() {
 		$field = 'username';
 		$msg1 = 'Error with the username';
 		$msg2 = 'Make the username nonempty';
-		
+
 		$form = new Form;
-		
+
 		$form->addMessage($field, $msg1);
 		$this->assertEquals($msg1, $form->message($field));
-		
+
 		$form->addMessage($field, $msg2);
 		$this->assertEquals($msg2, $form->message($field));
 	}
-	
+
 	public function testMessage_ReturnsNullForMissingMessage() {
 		$form = new Form;
 		$this->assertNull($form->message('username'));
 	}
-	
+
 	/**
 	 * @expectedException PHPUnit_Framework_Error
-	 * @dataProvider providerInvalidArray
 	 */
-	public function testAttachException_IsException($exception) {
+	public function testAttachException_IsException() {
 		$form = new Form;
-		$form->attachException($exception);
+		$form->attachException(NULL);
 	}
-	
+
 	public function testAttachException_CanAttachPrebuiltException() {
 		$msg = 'An error occurred';
 		$exception = new \Exception($msg);
-		
+
 		$form = new Form;
 		$this->assertTrue($form->attachException($exception) instanceof \Jolt\Form);
-		
+
 		$exception = $form->getException();
 		$this->assertEquals($msg, $exception->getMessage());
 	}
-	
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testAttachLoader_IsLoader() {
+		$form = new Form;
+		$form->attachLoader(NULL);
+	}
+
+	public function testAttachLoader_ReturnsJoltFormObject() {
+		$loader = $this->getMock('\Jolt\Form\Loader');
+		$form = new Form;
+
+		$this->assertTrue($form->attachLoader($loader) instanceof \Jolt\Form);
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testAttachWriter_IsWriter() {
+		$form = new Form;
+		$form->attachWriter(NULL);
+	}
+
+	public function testAttachWriter_ReturnsJoltFormObject() {
+		$loader = $this->getMock('\Jolt\Form\Writer');
+		$form = new Form;
+
+		$this->assertTrue($form->attachWriter($loader) instanceof \Jolt\Form);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public function testLoad_ReturnsFalseIfNoLoader() {
+
+	}
+
+	public function testWrite_ReturnsFalseIfNoWriter() {
+
+	}
+
+	public function testValidate_ReturnsFalseIfNoValidator() {
+
+	}
+
+
 	/**
 	 * @expectedException PHPUnit_Framework_Error
 	 * @dataProvider providerInvalidArray
@@ -57,7 +113,7 @@ class FormTest extends TestCase {
 		$form = new Form;
 		$form->setData($data);
 	}
-	
+
 	public function testSetData_ReturnsSelf() {
 		$form = new Form;
 		$this->assertTrue($form->setData(array()) instanceof \Jolt\Form);
@@ -70,5 +126,5 @@ class FormTest extends TestCase {
 			array(new \stdClass)
 		);
 	}
-	
+
 }
