7328cdc89c1aed99dbfe3b522121d7caedc273d9
Vic Cherubini <vmc@leftnode.com>
2010-10-18 05:24:18 -0500

    Added method count() to return the number of ruleSets added to the Validator.

--- Jolt/Form/Validator.php
+++ Jolt/Form/Validator.php
@@ -6,17 +6,17 @@ namespace Jolt\Form;
 class Validator {
 
 	private $ruleSets = array();
-	
+
 	public function __construct($ruleSets=array()) {
 		foreach ( $ruleSets as $field => $ruleSet ) {
 			$this->addRuleSet($field, $ruleSet);
 		}
 	}
-	
+
 	public function __destruct() {
 		$this->ruleSets = array();
 	}
-	
+
 	public function addRuleSet($field, \Jolt\Form\Validator\RuleSet $ruleSet) {
 		if ( empty($field) ) {
 			throw new \Jolt\Exception('the RuleSet field can not be empty');
@@ -24,13 +24,17 @@ class Validator {
 		$this->ruleSets[$field] = $ruleSet;
 		return $this;
 	}
-	
+
+	public function count() {
+		return count($this->ruleSets);
+	}
+
 	public function isEmpty() {
 		return ( 0 === count($this->ruleSets) );
 	}
-	
+
 	public function getRuleSets() {
 		return $this->ruleSets;
 	}
-	
+
 }
