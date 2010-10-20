a3d400e0d70cb19bbdfc49109a1c614a4a76683b
Vic Cherubini <vmc@leftnode.com>
2010-10-20 07:04:13 -0500

    Fixed the error that __toString() wasn't returning a string in the case of Location based headers. It now returns an empty string.

--- Jolt/Client.php
+++ Jolt/Client.php
@@ -6,71 +6,78 @@ namespace Jolt;
 /**
  * The client is what is responsible for sending rendered views, errors,
  * and/or headers back to the actual client.
- * 
+ *
  * @author vmc <vmc@leftnode.com>
  */
 class Client {
 
 	private $controller;
 	private $headersToSend = array();
-	
+
 	public function __construct() {
-		
+
 	}
 
 	public function __destruct() {
-		
+
 	}
-	
+
 	public function __toString() {
 		return $this->buildOutput();
 	}
-	
+
 	public function attachController(\Jolt\Controller $controller) {
 		$this->controller = clone $controller;
 		return $this;
 	}
-	
+
 	public function buildOutput() {
 		// Must have controller
 		if ( is_null($this->controller) ) {
 			return '';
 		}
-		
+
 		$controller = $this->controller;
-		
+
 		$responseCode = $controller->getResponseCode();
 		$contentType = $controller->getContentType();
-		
+
 		$controllerHeaderList = $controller->getHeaderList();
 		$headersList = headers_list();
-		
+
 		// Always remove the Content-Type header, let Jolt handle it
 		header_remove('Content-Type');
 		header('Content-Type: ' . $contentType, true, $responseCode);
-		
+
 		if ( defined('JOLT_VERSION') ) {
 			header('X-Framework: Jolt ' . JOLT_VERSION);
 		}
-		
-		// Go through the list of headers to send, if they exist in the 
+
+		// Go through the list of headers to send, if they exist in the
 		// $controllerHeaderList, unset them
 		foreach ( $headersList as $fullHeader ) {
 			foreach ( $controllerHeaderList as $header => $value ) {
 				if ( false !== stripos($fullHeader, $header) ) {
 					header_remove($header);
 				}
-				
-				header("{$header}: {$value}", true, $responseCode);
+
+				$header = strtolower(trim($header));
+				header($header . ':' . $value, true, $responseCode);
+
+				// Special case for the Location header to prevent __toString()
+				// from not outputting anything.
+				if ( 'location' === $header ) {
+					return '';
+				}
 			}
 		}
-		
+
 		$renderedController = $this->controller
 			->getRenderedController();
-		
+
 		return $renderedController;
 	}
-	
+
 	public function getController() {
 		return $this->controller;
 	}
