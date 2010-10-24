d0f540524c6b6b42d516fa4e5291adc13a62fa61
Vic Cherubini <vmc@leftnode.com>
2010-10-24 06:55:00 -0500

    Added new ability to register a javascript file in a view, and then render them all together in another view.

--- Jolt/View.php
+++ Jolt/View.php
@@ -5,11 +5,11 @@ namespace Jolt;
 
 /**
  * The basic class for rendering Views and inserting blocks/widgets into a view.
- * 
+ *
  * @author vmc <vmc@leftnode.com>
  */
 class View {
-	
+
 	// Settings
 	private $cssPath = NULL;
 	private $jsPath = NULL;
@@ -18,52 +18,54 @@ class View {
 	private $url = NULL;
 	private $useRewrite = false;
 	private $viewPath = NULL;
-	
+
 	private $renderedView = NULL;
+
 	private $variables = array();
+	private $javascripts = array();
 	private $blockList = array();
-	
+
 	const EXT = '.phtml';
-	
+
 	public function __construct() {
-		
+
 	}
-	
+
 	public function __destruct() {
-		
+
 	}
-	
+
 	public function __set($k, $v) {
 		$this->variables[$k] = $v;
 		return $this;
 	}
-	
+
 	public function __get($k) {
 		if ( isset($this->variables[$k]) ) {
 			return $this->variables[$k];
 		}
 		return NULL;
 	}
-	
+
 	public function addBlock($blockName, $block) {
 		$this->blockList[$blockName] = $block;
 		return $this;
 	}
-	
+
 	public function render($view) {
 		$view = $this->appendExtension($view, self::EXT);
-		
+
 		// Find the view file
 		$viewFile = $this->viewPath . $view;
 		if ( !is_file($viewFile) ) {
 			throw new \Jolt\Exception("View file '{$viewFile}' not found.");
 		}
-		
+
 		extract($this->variables);
 		ob_start();
 			require $viewFile;
 		$this->renderedView = ob_get_clean();
-		
+
 		return $this;
 	}
 
@@ -73,53 +75,79 @@ class View {
 
 	public function css($cssFile, $media='screen', $localFile=true) {
 		$cssFile = $this->appendExtension($cssFile, '.css');
-		
+
 		if ( $localFile ) {
 			$cssFile = DIRECTORY_SEPARATOR . $this->cssPath . $cssFile;
 		}
-		
+
 		$linkTag = sprintf('<link type="text/css" rel="stylesheet" href="%s" media="%s">%s', $cssFile, $media, PHP_EOL);
-		
+
 		return $linkTag;
 	}
-	
+
 	public function href($url, $text, $tagAttributes=NULL, $localUrl=true, $secure=false) {
 		if ( $localUrl ) {
 			$url = $this->url($url, $secure);
 		}
-		
+
 		$text = $this->safe($text);
 		$href = sprintf('<a href="%s" %s>%s</a>%s', $url, $tagAttributes, $text, PHP_EOL);
-		
+
 		return $href;
 	}
-	
+
 	public function img($imgSrc, $altText=NULL, $tagAttributes=NULL, $localFile=true) {
 		if ( $localFile ) {
 			$imgSrc = DIRECTORY_SEPARATOR . $this->imagePath . $imgSrc;
 		}
-		
+
 		$imgTag = sprintf('<img src="%s" alt="%s" title="%s" %s>%s', $imgSrc, $altText, $altText, $tagAttributes, PHP_EOL);
-		
+
 		return $imgTag;
 	}
-	
+
 	public function js($jsFile, $localFile=true) {
 		$jsFile = $this->appendExtension($jsFile, '.js');
-		
+
 		if ( $localFile ) {
 			$jsFile = DIRECTORY_SEPARATOR . $this->jsPath . $jsFile;
 		}
-		
+
 		$jsTag = sprintf('<script src="%s" type="text/javascript"></script>%s', $jsFile, PHP_EOL);
-		
+
 		return $jsTag;
 	}
-	
+
+	public function includeJs($type='script') {
+		$javascripts = $this->getJavascripts();
+
+		$includeJs = NULL;
+		foreach ( $javascripts as $script ) {
+			if ( array_key_exists($type, $script) ) {
+				$includeJs .= $script[$type];
+			}
+		}
+
+		return $includeJs;
+	}
+
+	public function registerJs($jsScript, $jsClass=NULL) {
+		$js = array();
+
+		$js['script'] = $this->js($jsScript);
+		if ( !empty($jsClass) ) {
+			$js['ready'] = '(new ' . $jsClass . '().ready());';
+		}
+
+		$this->javascripts[] = $js;
+
+		return $this;
+	}
+
 	public function url() {
 		$argc = func_num_args();
 		$argv = func_get_args();
-		
+
 		$urlPrefix = $this->url;
 		if ( $argc > 0 && is_bool($argv[$argc-1]) ) {
 			$secure = array_pop($argv);
@@ -128,10 +156,10 @@ class View {
 				$urlPrefix = $this->secureUrl;
 			}
 		}
-		
+
 		$p = $this->makeUrlParameters($argc, $argv);
 		$url = $urlPrefix . $p;
-		
+
 		return $url;
 	}
 
@@ -159,21 +187,21 @@ class View {
 		$this->secureUrl = trim($secureUrl);
 		return $this;
 	}
-	
+
 	public function setUrl($url) {
 		$this->url = trim($url);
 		return $this;
 	}
-	
+
 	public function setUseRewrite($useRewrite) {
 		if ( !is_bool($useRewrite) ) {
 			$useRewrite = false;
 		}
-		
+
 		$this->useRewrite = $useRewrite;
 		return $this;
 	}
-	
+
 	public function setViewPath($viewPath) {
 		$this->viewPath = $this->appendDirectorySeparator($viewPath);
 		return $this;
@@ -182,7 +210,7 @@ class View {
 	public function getBlockList() {
 		return $this->blockList;
 	}
-	
+
 	public function getBlock($blockName) {
 		if ( isset($this->blockList[$blockName]) ) {
 			return $this->blockList[$blockName];
@@ -205,29 +233,33 @@ class View {
 	public function getRenderedView() {
 		return $this->renderedView;
 	}
-	
+
 	public function getRouteParameter() {
 		return $this->routeParameter;
 	}
-	
+
 	public function getSecureUrl() {
 		return $this->secureUrl;
 	}
-	
+
 	public function getUrl() {
 		return $this->url;
 	}
-	
+
 	public function getUseRewrite() {
 		return $this->useRewrite;
 	}
-	
+
 	public function getViewPath() {
 		return $this->viewPath;
 	}
-	
+
 	public function getVariables() {
 		return $this->variables;
+	}
+
+	public function getJavascripts() {
+		return $this->javascripts;
 	}
 
 	private function appendExtension($file, $ext) {
@@ -236,7 +268,7 @@ class View {
 		}
 		return $file;
 	}
-	
+
 	private function appendDirectorySeparator($path) {
 		$pathLength = strlen(trim($path)) - 1;
 		if ( $path[$pathLength] != DIRECTORY_SEPARATOR ) {
@@ -249,19 +281,19 @@ class View {
 		if ( 0 == $argc ) {
 			return NULL;
 		}
-		
+
 		$route = NULL;
 		$root = $argv[0];
 		if ( $argc > 1 ) {
 			$route = '/' . implode('/', array_slice($argv, 1));
 		}
-		
+
 		$parameters = $root . $route;
 		if ( !$this->useRewrite ) {
 			$parameters = "index.php?{$this->routeParameter}={$parameters}";
 		}
-		
+
 		return $parameters;
 	}
-	
+
 }
