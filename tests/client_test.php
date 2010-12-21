162b1e27a707c29e8c38eac82298c45a0dd54c5a
Vic Cherubini <vmc@leftnode.com>
2010-12-21 06:23:33 -0600

    Achieved 100% test coverage by mocking the \jolt\controller objects properly.
    * The setUp() method no longer builds a mock controller, I believe this
      is due to a bug in PHPUnit. After building the $this->controller
      object in setUp(), the test_attach_controller__requires_jolt_controller()
      method would throw a bunch of failures for not using the methods in
      the mocked \jolt\controller object the expected number of times.
      This will need to be fixed in future versions of my code and PHPUnit.
    * Lowercased and used UNIX naming conventions for all variables and
      method names.

--- tests/client_test.php
+++ tests/client_test.php
@@ -1,56 +1,42 @@
 <?php
 
 declare(encoding='UTF-8');
-namespace JoltTest;
+namespace jolt_test;
 
-use \Jolt\Client,
-	\JoltTest\TestCase;
+use \jolt\client,
+	\jolt_test\testcase;
 
-require_once 'jolt/client.php';
-require_once 'jolt/controller.php';
+class client_test extends testcase {
 
-class ClientTest extends TestCase {
-
-	private $client = NULL;
+	private $controller = NULL;
+	private $output = '';
 
 	public function setUp() {
-		$this->client = $this->getMock('\Jolt\Client', array('sendHeader'));
-		$this->client->expects($this->any())
-			->method('sendHeader')
-			->will($this->returnValue(true));
+		//$this->controller = $this->getMock('\jolt\controller', array('get_response_code'));
+		//$this->controller->expects($this->once())->method('get_response_code')->will($this->returnValue(200));
 	}
 
 	/**
 	 * @expectedException PHPUnit_Framework_Error
 	 * @dataProvider providerInvalidJoltController
 	 */
-	public function testAttachController_IsJoltController($controller) {
-		$client = new Client;
-		$client->attachController($controller);
+	public function test_attach_controller__requires_jolt_controller($controller) {
+		$client = new client;
+		$client->attach_controller($controller);
 	}
 
-	public function testAttachController_IsAttached() {
-		$actualController = $this->buildMockController();
+	public function test_attach_controller__is_attached() {
+		$actual_controller = $this->getMock('\jolt\controller');
 
-		$client = new Client;
-		$client->attachController($actualController);
-		$expectedController = $client->getController();
+		$client = new client;
+		$client->attach_controller($actual_controller);
+		$expected_controller = $client->get_controller();
 
-		$this->assertTrue($expectedController instanceof $actualController);
+		$this->assertTrue($expected_controller instanceof $actual_controller);
 	}
 
-	/**
-	 * @expectedException PHPUnit_Framework_Error
-	 */
-	public function _testSendHeader_SendsHeader() {
-		$client = new Client;
-
-		// PHPUnit sends a header for some reason and thus, we expect an error here.
-		$client->sendHeader('X-Fake-Header', 'abc');
-	}
-
-	public function test__ToString_ControllerAttached() {
-		$client = new Client;
+	public function test___ToString__is_empty_without_controller_attached() {
+		$client = new client;
 
 		ob_start();
 			echo $client;
@@ -59,24 +45,80 @@ class ClientTest extends TestCase {
 		$this->assertEmpty($output);
 	}
 
-	public function test__ToString_SendsHeaders() {
-		$controller = $this->buildMockController();
-		$controller->addHeader('X-Powered-By', 'Jolt/PHP');
+	public function test_build_output__sends_headers() {
+		$output = 'fin jolt controller';
 
-		$this->client->attachController($controller);
+		$controller = $this->getMock('\jolt\controller', array('get_response_code', 'get_content_type', 'get_headers', 'get_rendered_controller'));
+		$controller->expects($this->once())
+			->method('get_response_code')
+			->will($this->returnValue(200));
 
-		$output = $this->client->buildOutput();
-		$this->assertEmpty($output);
+		$controller->expects($this->once())
+			->method('get_content_type')
+			->will($this->returnValue('text/html'));
+
+		$controller->expects($this->once())
+			->method('get_headers')
+			->will($this->returnValue(array('x-fake-header' => 'jolt rocks')));
+
+		$controller->expects($this->once())
+			->method('get_rendered_controller')
+			->will($this->returnValue($output));
+
+		$client = new client;
+		$client->attach_controller($controller);
+
+		$this->assertEquals($output, $client->build_output());
 	}
 
-	public function test__ToString_SendsNonSentHeaders() {
-		$controller = $this->buildMockController();
-		$controller->addHeader('X-Powered-By', 'Jolt/PHP');
+	public function test_build_output__removes_overridden_headers() {
+		$output = 'fin jolt controller';
 
-		$this->client->attachController($controller);
+		$controller = $this->getMock('\jolt\controller', array('get_response_code', 'get_content_type', 'get_headers', 'get_rendered_controller'));
+		$controller->expects($this->once())
+			->method('get_response_code')
+			->will($this->returnValue(200));
 
-		$output = $this->client->buildOutput();
-		$this->assertEmpty($output);
+		$controller->expects($this->once())
+			->method('get_content_type')
+			->will($this->returnValue('text/html'));
+
+		$controller->expects($this->once())
+			->method('get_headers')
+			->will($this->returnValue(array('x-fake-header' => 'jolt rocks')));
+
+		$controller->expects($this->once())
+			->method('get_rendered_controller')
+			->will($this->returnValue($output));
+
+		$client = new client;
+		$client->attach_controller($controller);
+		$client->set_headers(array('x-fake-header: jolt is pretty cool'));
+
+		$this->assertEquals($output, $client->build_output());
+	}
+
+	public function test_build_output__returns_empty_string_for_location_header() {
+		$output = 'fin jolt controller';
+
+		$controller = $this->getMock('\jolt\controller', array('get_response_code', 'get_content_type', 'get_headers'));
+		$controller->expects($this->once())
+			->method('get_response_code')
+			->will($this->returnValue(200));
+
+		$controller->expects($this->once())
+			->method('get_content_type')
+			->will($this->returnValue('text/html'));
+
+		$controller->expects($this->once())
+			->method('get_headers')
+			->will($this->returnValue(array('location' => 'http://joltcore.org')));
+
+		$client = new client;
+		$client->attach_controller($controller);
+		$client->set_headers(array('x-fake-header: jolt rocks'));
+
+		$this->assertEmpty($client->build_output());
 	}
 
 	public function providerInvalidJoltController() {
@@ -85,7 +127,7 @@ class ClientTest extends TestCase {
 			array(10),
 			array(array('a')),
 			array(new \stdClass),
-			array(new Client)
+			array(new client)
 		);
 	}
 }
