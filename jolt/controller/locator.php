1dadfbc5480a623cfa21689d247b58c72ef4ed16
Vic Cherubini <vmc@leftnode.com>
2011-01-22 09:15:02 -0600

    Made this much more succinct and changed the load() method to find().

--- jolt/controller/locator.php
+++ jolt/controller/locator.php
@@ -5,11 +5,6 @@ namespace jolt\controller;
 
 class locator {
 
-	private $file = NULL;
-	private $path = NULL;
-
-	const EXT = '.php';
-
 	public function __construct() {
 
 	}
@@ -18,58 +13,29 @@ class locator {
 
 	}
 
-	public function load($path, $controller) {
-		$bits = explode('\\', $controller);
-		$bits_length = count($bits);
-
-		$this->file = $bits[$bits_length-1];
-		$this->file = $this->convert_underscores_to_dashes($this->file);
-		$this->path = $path;
-
-		if (0 === preg_match('/\\' . self::EXT . '$/i', $controller)) {
-			$this->file .= self::EXT;
-		}
-
-		$path_length = strlen($path);
-		if ($path[$path_length-1] !== DIRECTORY_SEPARATOR) {
-			$this->path .= DIRECTORY_SEPARATOR;
-		}
-
-		$controller_path = $this->path . $this->file;
+	public function find($controller_path, $controller) {
 		if (!is_file($controller_path)) {
-			throw new \jolt\exception('controller path not found');
+			throw new \jolt\exception('Controller path '.$controller_path.' not found.');
 		}
+
+$classes1 = get_declared_classes();
 
 		require_once($controller_path);
 
+$classes2 = get_declared_classes();
+print_r(array_diff($classes2, $classes1));
+
 		if (!class_exists($controller))  {
-			throw new \jolt\exception('controller class ' . $controller . ' does not exist');
+			throw new \jolt\exception('Controller class '.$controller.' does not exist.');
 		}
 
 		$controller_object = new $controller;
 
 		if (!($controller_object instanceof \jolt\controller)) {
-			throw new \jolt\exception('controller_locator_controller_not_instance_of_controller' . $controller);
+			throw new \jolt\exception('Controller class '.$controller.' is not an instance of \jolt\controller.');
 		}
 
 		return $controller_object;
-	}
-
-	public function get_path() {
-		return $this->path;
-	}
-
-	public function get_file() {
-		return $this->file;
-	}
-
-	private function convert_underscores_to_dashes($v) {
-		//$v = strtolower(substr($v, 0, 1)) . substr($v, 1);
-		//$v = preg_replace('/[A-Z]/', '-\\0', $v);
-		//$v = strtolower($v);
-		$v = strtolower($v);
-		$v = str_replace('_', '-', $v);
-		return $v;
 	}
 
 }
