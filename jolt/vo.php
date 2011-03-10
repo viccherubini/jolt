c050d81333548a6275dc7da77c1de6642adea2a6
Vic Cherubini <vmc@leftnode.com>
2011-03-10 04:30:01 -0600

    Removed the length property because it can't be quickly and accurately calculated without maintaining a separate array of values.

--- jolt/vo.php
+++ jolt/vo.php
@@ -5,18 +5,15 @@ namespace jolt;
 
 class vo {
 
-	private $length = 0;
-
 	public function __construct($array=array()) {
-		foreach ($array as $k => $v) {
-			$this->__set($k, $v);
+		if (is_array($array)) {
+			foreach ($array as $k => $v) {
+				$this->__set($k, $v);
+			}
 		}
 	}
 
 	public function __set($k, $v) {
-		if (!property_exists($this, $k)) {
-			$this->length++;
-		}
 		$this->$k = $v;
 		return true;
 	}
@@ -24,10 +21,6 @@ class vo {
 	public function __get($k) {
 		$retval = (!property_exists($this, $k) ? NULL : $this->$k);
 		return $retval;
-	}
-
-	public function length() {
-		return $this->length;
 	}
 
 }
