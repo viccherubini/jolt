34f677cb0eff263f78b142fcf1b8646e70d7876c
Vic Cherubini <vmc@leftnode.com>
2011-04-08 05:49:31 -0500

    Added a new private array of keys added to the value object. This allowed for a method to_array() to be made that returns all keys added to the value object to be returned as a key/value array.

--- jolt/vo.php
+++ jolt/vo.php
@@ -5,6 +5,8 @@ namespace jolt;
 
 class vo {
 
+	private $_value_keys = array();
+
 	public function __construct($array=array()) {
 		if (is_array($array)) {
 			foreach ($array as $k => $v) {
@@ -15,6 +17,10 @@ class vo {
 
 	public function __set($k, $v) {
 		$this->$k = $v;
+		if (!array_key_exists($k, $this->_value_keys)) {
+			$this->_value_keys[$k] = true;
+		}
+
 		return true;
 	}
 
@@ -23,4 +29,14 @@ class vo {
 		return $retval;
 	}
 
+	public function to_array() {
+		$value_array = array();
+		foreach ($this as $k => $v) {
+			if (array_key_exists($k, $this->_value_keys)) {
+				$value_array[$k] = $v;
+			}
+		}
+
+		return $value_array;
+	}
 }
