e4be5e01e8a063c16c3b78574be38672dec7c63c
Vic Cherubini <vmc@leftnode.com>
2011-07-15 05:57:53 -0500

    Moved the session based loading and writing code to these objects, they are also no non-abstract.

--- jolt/form/loader.php
+++ jolt/form/loader.php
@@ -5,8 +5,74 @@ namespace jolt\form;
 
 require_once('jolt/form_controller.php');
 
-abstract class loader extends \jolt\form_controller {
+class loader extends \jolt\form_controller {
 
-	abstract public function load();
+	private $session;
+	private $key = 'jolt.form';
+
+	public function attach_session(\jolt\session $session) {
+		$this->session = $session;
+		return $this;
+	}
+
+	public function load() {
+		$session = $this->get_session();
+		if (is_null($session)) {
+			return false;
+		}
+
+		$id = $this->get_id();
+		if (empty($id)) {
+			return false;
+		}
+
+		$name = $this->get_name();
+		if (empty($name)) {
+			return false;
+		}
+
+		$key = $this->get_key();
+		if (isset($session->$key)) {
+			$jolt_forms = $session->$key;
+			if (array_key_exists($name, $jolt_forms)) {
+				$form_data = $jolt_forms[$name];
+
+				$data = json_decode($form_data['data'], true);
+				$errors = json_decode($form_data['errors'], true);
+
+				if (is_null($data)) {
+					$data = array($form_data['data']);
+				}
+
+				if (is_null($errors)) {
+					$errors = array($form_data['errors']);
+				}
+
+				$this->set_data($data)
+					->set_errors($errors)
+					->set_message($form_data['message']);
+
+				unset($jolt_forms[$name]);
+				$session->$key = $jolt_forms;
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function set_key($key) {
+		$this->key = trim($key);
+		return $this;
+	}
+
+	public function get_key() {
+		return $this->key;
+	}
+
+	public function get_session() {
+		return $this->session;
+	}
 
 }
--- jolt/form/writer.php
+++ jolt/form/writer.php
@@ -5,8 +5,63 @@ namespace jolt\form;
 
 require_once('jolt/form_controller.php');
 
-abstract class writer extends \jolt\form_controller {
+class writer extends \jolt\form_controller {
 
-	abstract public function write();
+	private $session = NULL;
+	private $key = 'jolt.form';
+
+	public function attach_session(\jolt\session $session) {
+		$this->session = $session;
+		return $this;
+	}
+
+	public function write() {
+		$session = $this->get_session();
+		if (is_null($session)) {
+			return false;
+		}
+
+		$data = $this->get_data();
+		$id = $this->get_id();
+		if (empty($id)) {
+			return false;
+		}
+
+		$name = $this->get_name();
+		$errors = $this->get_errors();
+		$message = $this->get_message();
+
+		$data_json = json_encode($data);
+		$errors_json = json_encode($errors);
+		
+		$jolt_forms = array();
+		$key = $this->get_key();
+		if (isset($session->$key)) {
+			$jolt_forms = $session->$key;
+		}
+
+		$jolt_forms[$name] = array(
+			'data' => $data_json,
+			'errors' => $errors_json,
+			'message' => $message
+		);
+
+		$session->$key = $jolt_forms;
+
+		return true;
+	}
+
+	public function set_key($key) {
+		$this->key = trim($key);
+		return $this;
+	}
+
+	public function get_key() {
+		return $this->key;
+	}
+
+	public function get_session() {
+		return $this->session;
+	}
 
 }
